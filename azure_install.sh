2e0730a7121c74f64096929f7c9d614c6b22d594
Ionut Cristian Paraschiv <ionut.paraschiv@3angletech.com>
2016-01-26 17:28:14 +0200

    updated git clone user for azure

--- azure_install.sh
+++ azure_install.sh
@@ -2,7 +2,7 @@
 
 mkdir azure
 cd azure
-git clone https://gitlab.3angletech.com/UnitPalDK/UnitPal.git
+git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git
 cd UnitPal
 (git remote add azure https://unitpalprod:UnPL3%242_2%40suT21@unitpalprod.scm.azurewebsites.net:443/UnitPalProd.git) || echo "Azure remote already added"
 git push azure master
