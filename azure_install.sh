3ddc80a0d4e0a675feb75d26fc7769e226cf0efb
Ionut Cristian Paraschiv <ionut.paraschiv@3angletech.com>
2016-01-27 12:16:31 +0200

    updated azure install

--- azure_install.sh
+++ azure_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# mkdir azure
-# cd azure
-# git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git
-# cd UnitPal
+mkdir azure
+cd azure
+git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git --branch master
+cd UnitPal
 (git remote add azure https://unitpalprod:UnPL3%242_2%40suT21@unitpalprod.scm.azurewebsites.net:443/UnitPalProd.git) || echo "Azure remote already added"
 git push azure master
-# cd ../../
-# rm -rf azure
+cd ../../
+rm -rf azure
 echo "Finished install on Azure"
