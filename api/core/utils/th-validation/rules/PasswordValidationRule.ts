c81e62de466bac9ca0365ff66a211987d72e4e31
Ionut Cristian Paraschiv <ionut.paraschiv@3angletech.com>
2016-02-11 14:24:39 +0200

    Account tests

--- api/core/utils/th-validation/rules/PasswordValidationRule.ts
+++ api/core/utils/th-validation/rules/PasswordValidationRule.ts
@@ -3,12 +3,10 @@ import {InvalidConstraint} from './core/ValidationResult';
 import {StringValidationRule} from './StringValidationRule';
 
 export class PasswordValidationRule extends AValidationRule {
-	private static MinLength = 6;
 	private _stringValidationRule: StringValidationRule;
 	constructor() {
 		super(InvalidConstraint.Password);
-		this._stringValidationRule = new StringValidationRule(Number.POSITIVE_INFINITY);
-		this._stringValidationRule.minLength = PasswordValidationRule.MinLength;
+		this._stringValidationRule = new StringValidationRule();
 	}
 	protected validateCore(object: any): boolean {
 		var stringValidationResult = this._stringValidationRule.validate(object);
@@ -17,16 +15,8 @@ export class PasswordValidationRule extends AValidationRule {
 		}
 		return this.validateByPasswordRegex(object);
 	}
-	
-	/*
-		(?=.*[a-z]) - lower case regex
-		(?=.*[A-Z]) - upper case regex
-		(?=.*\d) - digit regex
-		.+ - each set at least once
-		$ - end of the string
-	*/
 	private validateByPasswordRegex(passwd: string): boolean {
-		var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/i;
+		var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{6,}$/;
 		return re.test(passwd);
 	}
 }
