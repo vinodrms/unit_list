99fbff6b810e751c646fa4eb52439189566f0e8e
Dragos Pricope <dragos.pricope@3angle.tech>
2016-04-15 11:17:38 +0300

    timezone fix

--- api/test/root/integration-tests/time-zones/TimeZonesIntegrationTest.ts
+++ api/test/root/integration-tests/time-zones/TimeZonesIntegrationTest.ts
@@ -5,7 +5,7 @@ import {TestContext} from '../../../helpers/TestContext';
 import {ITimeZonesService, TimeZoneDO} from '../../../../core/services/time-zones/ITimeZonesService';
 
 describe("Time Zones Integration Tests", function() {
-    const numTimeZones = 586;
+    const numTimeZones = 506;
     
     var testContext: TestContext;
     var timeZonesService: ITimeZonesService;
