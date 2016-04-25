6f5fed183fc0bf0a62dbf28b5a086aa97fdbb7f0
Dragos Pricope <dragos.pricope@3angle.tech>
2016-04-25 16:13:51 +0300

    azure remote git repo changed

--- azure_install.sh
+++ azure_install.sh
@@ -4,7 +4,7 @@ mkdir azure
 cd azure
 git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git --branch master
 cd UnitPal
-(git remote add azure https://unitpalth:UnPL3%242_2%40suT21@unitpal2.scm.azurewebsites.net:443/UnitPal2.git) || echo "Azure remote already added"
+(git remote add azure https://unitpalth:UnPL3%242_2%40suT21@unitpal.scm.azurewebsites.net:443/UnitPal.git) || echo "Azure remote already added"
 git push azure master
 cd ../../
 rm -rf azure
