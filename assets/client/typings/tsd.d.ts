7962f9c415a8194c9cd6f57b5ffa9dab14d99bd5
Ionut Cristian Paraschiv <ionut.paraschiv@3angletech.com>
2016-05-03 13:31:28 +0300

    update

--- assets/client/typings/tsd.d.ts
+++ assets/client/typings/tsd.d.ts
@@ -1,17 +1,8 @@
-/* 
-**	Added manually - needed to build Angular
-*/
-
-/// <reference path="../../node_modules/angular2/typings/browser.d.ts" />
-
-/* 
-**	Added with tsd command
-**	e.g.: tsd install % --save 
-*/
-
 /// <reference path="moment/moment-node.d.ts" />
 /// <reference path="moment/moment.d.ts" />
 /// <reference path="systemjs/systemjs.d.ts" />
 /// <reference path="underscore/underscore.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="toastr/toastr.d.ts" />
+/// <reference path="es6-shim/es6-shim.d.ts" />
+/// <reference path="jasmine/jasmine.d.ts" />
