6bc411676c011067d5886de6270a7215998988b1
Ionut Cristian Paraschiv <ionut.paraschiv@3angletech.com>
2016-05-20 08:43:44 +0300

    [T203] Hotel Operations Bootstrap

--- assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/container/HotelOperationsContainerComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/container/HotelOperationsContainerComponent.ts
@@ -1,11 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {TranslationPipe} from '../../../../../../../../common/utils/localization/TranslationPipe';
+import {ThButtonComponent} from '../../../../../../../../common/utils/components/ThButtonComponent';
 import {HeaderPageService} from '../../../utils/header/services/HeaderPageService';
 import {HeaderPageType} from '../../../utils/header/services/HeaderPageType';
 import {AHomeContainerComponent} from '../../../utils/AHomeContainerComponent';
 
 @Component({
 	selector: 'hotel-operations-container',
-	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/container/template/hotel-operations-container.html'
+	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/container/template/hotel-operations-container.html',
+	directives: [ThButtonComponent],
+	pipes: [TranslationPipe]
 })
 
 export class HotelOperationsContainerComponent extends AHomeContainerComponent implements OnInit {
@@ -15,5 +19,9 @@ export class HotelOperationsContainerComponent extends AHomeContainerComponent i
 	}
 
 	ngOnInit() {
+	}
+
+	openNewBookingModal() {
+		// TODO: new booking modal
 	}
 }
