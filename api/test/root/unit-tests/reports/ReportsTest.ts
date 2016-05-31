9b66efbecc678f04677cf24588ee2ff5bd697841
Dragos Pricope <dragos.pricope@3angle.tech>
2016-05-31 13:37:57 +0300

    Added tests for the html report service

---
+++ api/test/root/unit-tests/reports/ReportsTest.ts
@@ -0,0 +1,54 @@
+require("sails-test-helper");
+import should = require('should');
+import supertest = require('supertest');
+
+import {ThError} from '../../../../core/utils/th-responses/ThError';
+import {ThStatusCode} from '../../../../core/utils/th-responses/ThResponse';
+import {DefaultDataBuilder} from '../../../db-initializers/DefaultDataBuilder';
+import {TestContext} from '../../../helpers/TestContext';
+import {TestUtils} from '../../../../test/helpers/TestUtils';
+import {ReportsTestHelper} from './helpers/ReportsTestHelper';
+
+import {HtmlReportsServiceResponse} from '../../../../core/services/html-reports/IHtmlReportsService';
+import fs = require("fs");
+
+describe("PDF Reports Tests", function () {
+    var testUtils: TestUtils;
+    var testContext: TestContext;
+    var testDataBuilder: DefaultDataBuilder;
+    var testHelper: ReportsTestHelper;
+
+    before(function (done: any) {
+        testUtils = new TestUtils();
+        testContext = new TestContext();
+        testDataBuilder = new DefaultDataBuilder(testContext);
+        testDataBuilder.buildWithDoneCallback(done);
+        testHelper = new ReportsTestHelper(testDataBuilder);
+    });
+
+    describe("PDF Reports Generation Flow", function () {
+        it("Should generate an invoice pdf", function (done) {
+            var serviceFactory = testContext.appContext.getServiceFactory();
+            var htmlReportsService = serviceFactory.getHtmlReportsService(serviceFactory.getHtmltoPdfConverterService());
+            htmlReportsService.generateReport(testHelper.getInvoicePdfReportRequest()).then((response: HtmlReportsServiceResponse) => {
+                fs.exists(response.pdfPath, (exists) => {
+                    exists ? done() : done(response.pdfPath + ' not found!');
+                });
+            }).catch((err: any) => {
+                done(err);
+            });
+
+        });
+        it("Should generate a booking confirmation pdf", function (done) {
+            var serviceFactory = testContext.appContext.getServiceFactory();
+            var htmlReportsService = serviceFactory.getHtmlReportsService(serviceFactory.getHtmltoPdfConverterService());
+            htmlReportsService.generateReport(testHelper.getBookingConfirmationPdfReportRequest()).then((response: HtmlReportsServiceResponse) => {
+                fs.exists(response.pdfPath, (exists) => {
+                    exists ? done() : done(response.pdfPath + ' not found!');
+                });
+            }).catch((err: any) => {
+                done(err);
+            });
+        });
+    });
+});
