dd8be3a5040e4c28aa0d4a1f640317c7fc3f1e2c
Dragos Pricope <dragos.pricope@3angle.tech>
2016-06-29 13:34:40 +0300

    added week days strings in ISOWeekDay

--- api/core/utils/th-dates/data-objects/ISOWeekDay.ts
+++ api/core/utils/th-dates/data-objects/ISOWeekDay.ts
@@ -7,7 +7,10 @@ export enum ISOWeekDay {
 	Saturday,
 	Sunday
 }
-
+export interface ISOWeekDayVM {
+	iSOWeekDay: ISOWeekDay,
+	name: string
+}
 export class ISOWeekDayUtils {
 	public getISOWeekDayList(): ISOWeekDay[] {
 		return [
@@ -19,5 +22,37 @@ export class ISOWeekDayUtils {
 			ISOWeekDay.Saturday,
 			ISOWeekDay.Sunday
 		];
+	}
+	public getISOWeekDayVMList(): ISOWeekDayVM[] {
+		return [
+			{
+				iSOWeekDay: ISOWeekDay.Monday,
+				name: "Monday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Tuesday,
+				name: "Tuesday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Wednesday,
+				name: "Wednesday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Thursday,
+				name: "Thursday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Friday,
+				name: "Friday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Saturday,
+				name: "Saturday"
+			},
+			{
+				iSOWeekDay: ISOWeekDay.Sunday,
+				name: "Sunday"
+			}
+		]
 	}
 }
