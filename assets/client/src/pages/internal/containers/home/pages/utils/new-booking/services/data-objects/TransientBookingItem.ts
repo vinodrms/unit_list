6b377652cc31354e73ce879e6c42d5120563822e
Ionut Paraschiv <paraschiv.ionut@gmail.com>
2016-07-02 16:56:51 +0300

    [T206] Added missing DOs for TransientBookingItem

--- assets/client/src/pages/internal/containers/home/pages/utils/new-booking/services/data-objects/TransientBookingItem.ts
+++ assets/client/src/pages/internal/containers/home/pages/utils/new-booking/services/data-objects/TransientBookingItem.ts
@@ -1,10 +1,14 @@
 import {ThDateIntervalDO} from '../../../../../../../services/common/data-objects/th-dates/ThDateIntervalDO';
 import {ConfigCapacityDO} from '../../../../../../../services/common/data-objects/bed-config/ConfigCapacityDO';
+import {DefaultBillingDetailsDO} from '../../../../../../../services/bookings/data-objects/default-billing/DefaultBillingDetailsDO';
 
 export class TransientBookingItem {
     interval: ThDateIntervalDO;
     configCapacity: ConfigCapacityDO;
+    customerIdList: string[];
+    defaultBillingDetails: DefaultBillingDetailsDO;
     roomCategoryId: string;
     priceProductId: string;
     allotmentId: string;
+    notes: string;
 }
