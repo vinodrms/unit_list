e23dce00101c30c2d593e7cc8ba841cc616936eb
Dragos Pricope <dragos.pricope@3angle.tech>
2016-08-01 20:12:23 +0300

    remove invoice API from ThServerApi

--- assets/client/src/common/utils/http/ThServerApi.ts
+++ assets/client/src/common/utils/http/ThServerApi.ts
@@ -91,7 +91,6 @@ export enum ThServerApi {
 	InvoiceGroups,
 	InvoiceGroupsCount,
 	InvoiceGroupItem,
-	Invoice,
 }
 
 var ThServerApiUrl: { [index: number]: string; } = {};
@@ -190,7 +189,6 @@ ThServerApiUrl[ThServerApi.HotelOperationsCommonSendEmail] = "/hotelOperations/c
 ThServerApiUrl[ThServerApi.InvoiceGroups] = "/invoiceGroups";
 ThServerApiUrl[ThServerApi.InvoiceGroupsCount] = "/invoiceGroups/count";
 ThServerApiUrl[ThServerApi.InvoiceGroupItem] = "/invoiceGroups/invoiceGroupItem";
-ThServerApiUrl[ThServerApi.Invoice] = "/invoice";
 
 export class ServerApiBuilder {
 	public static ApiRoot = "/api";
