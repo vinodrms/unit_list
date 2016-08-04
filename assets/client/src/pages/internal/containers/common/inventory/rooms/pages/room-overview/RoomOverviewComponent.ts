c4ccd067dbcb8e1cab668d204228ff8acbca2640
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-08-04 14:48:41 +0300

    [T256] integrated operational modals in settings

--- assets/client/src/pages/internal/containers/common/inventory/rooms/pages/room-overview/RoomOverviewComponent.ts
+++ assets/client/src/pages/internal/containers/common/inventory/rooms/pages/room-overview/RoomOverviewComponent.ts
@@ -3,31 +3,45 @@ import {BaseComponent} from '../../../../../../../../common/base/BaseComponent';
 import {TranslationPipe} from '../../../../../../../../common/utils/localization/TranslationPipe';
 import {RoomVM} from '../../../../../../services/rooms/view-models/RoomVM';
 import {CustomScroll} from '../../../../../../../../common/utils/directives/CustomScroll';
+import {ModalDialogRef} from '../../../../../../../../common/utils/modals/utils/ModalDialogRef';
+import {HotelOperationsModalService} from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/HotelOperationsModalService';
+import {HotelOperationsResult} from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/utils/HotelOperationsResult';
 
 @Component({
     selector: 'room-overview',
     templateUrl: '/client/src/pages/internal/containers/common/inventory/rooms/pages/room-overview/template/room-overview.html',
-    pipes: [TranslationPipe],
+    providers: [HotelOperationsModalService],
+	pipes: [TranslationPipe],
 	directives: [CustomScroll]
 })
 export class RoomOverviewComponent extends BaseComponent {
-    constructor() { 
+	@Input() showLinkToOperationalModal: boolean = false;
+
+    constructor(private _hotelOperationsModalService: HotelOperationsModalService) {
         super();
     }
-    
+
     private _roomVM: RoomVM;
 	public get roomVM(): RoomVM {
 		return this._roomVM;
 	}
-    
+
 	@Input()
 	public set roomVM(roomVM: RoomVM) {
 		this._roomVM = roomVM;
 	}
-    
+
     @Output() onEdit = new EventEmitter();
 	public editRoom() {
 		this.onEdit.next(this._roomVM);
 	}
 
+	public openRoomOperationsModal() {
+		this._hotelOperationsModalService.openRoomOperationsModal(this._roomVM.room.id).then((modalDialogRef: ModalDialogRef<HotelOperationsResult>) => {
+			modalDialogRef.resultObservable
+				.subscribe((result: HotelOperationsResult) => {
+				}, (err: any) => {
+				});
+		}).catch((err: any) => { });
+	}
 }
