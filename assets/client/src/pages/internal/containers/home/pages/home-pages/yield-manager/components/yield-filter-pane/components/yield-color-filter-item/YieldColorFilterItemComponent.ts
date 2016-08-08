bd248aefd86041d73906e8da03adeb918592289c
Dobre Catalin Andrei <catalin.dobre@3angletech.com>
2016-08-08 14:03:24 +0300

    [T258] Some renamings

--- assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-color-filter-item/YieldColorFilterItemComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-color-filter-item/YieldColorFilterItemComponent.ts
@@ -3,11 +3,11 @@ import { Component, OnInit, Input} from '@angular/core';
 import {ColorFilterVM}  from '../../../../../../../../../services/yield-manager/dashboard/filter/view-models/ColorFilterVM';
 
 @Component({
-	selector: 'yield-group-item',
-	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-group-item/template/yield-group-item.html'
+	selector: 'yield-color-filter-item',
+	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-color-filter-item/template/yield-color-filter-item.html'
 })
-export class YieldGroupItemComponent implements OnInit {
-	@Input() yieldGroupItemVM: ColorFilterVM;
+export class YieldColorFitlerItemComponent implements OnInit {
+	@Input() yieldColorFilterItemVM: ColorFilterVM;
 	public selected: boolean;
 	constructor() { }
 
--- assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-text-filter-item/YieldTextFilterItemComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-text-filter-item/YieldTextFilterItemComponent.ts
@@ -3,10 +3,10 @@ import { Component, OnInit, Input } from '@angular/core';
 import {TextFilterVM}  from '../../../../../../../../../services/yield-manager/dashboard/filter/view-models/TextFilterVM';
 
 @Component({
-	selector: 'yield-level-item',
-	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-level-item/template/yield-level-item.html'
+	selector: 'yield-text-filter-item',
+	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/components/yield-filter-pane/components/yield-text-filter-item/template/yield-text-filter-item.html'
 })
-export class YieldLevelItemComponent implements OnInit {
+export class YieldTextFilterItemComponent implements OnInit {
 	@Input() yieldLevelItemVM: TextFilterVM
 	public selected: boolean;
 	constructor() { }
