36814717754bbe07b44165d00d048441de2acd55
Dobre Catalin Andrei <catalin.dobre@3angletech.com>
2016-08-12 11:26:24 +0300

    [T258] - Added yield view mode

--- assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/YieldManagerDashboardComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/YieldManagerDashboardComponent.ts
@@ -7,7 +7,8 @@ import {AHomeContainerComponent} from '../../../utils/AHomeContainerComponent';
 import {YieldFilterPaneComponent} from './components/yield-filter-pane/YieldFilterPaneComponent';
 import {YieldKeyMetricsComponent} from './components/yield-key-metrics/YieldKeyMetricsComponent';
 import {YieldPriceProductsComponent} from './components/yield-price-products/YieldPriceProductsComponent';
-
+import {YieldViewModeComponent} from './components/yield-view-mode/YieldViewModeComponent';
+import {YieldViewModeState} from './components/yield-view-mode/YieldViewModeComponent';
 
 import {YieldFiltersService} from '../../../../../../services/hotel-configurations/YieldFiltersService';
 import {YieldManagerDashboardFilterService} from '../../../../../../services/yield-manager/dashboard/filter/YieldManagerDashboardFilterService';
@@ -24,19 +25,47 @@ export interface IYieldManagerDashboardPriceProducts {
 
 }
 
+class ViewModeDecoratorClass{
+	public static Default = '';
+	public static Shrinked = 'shrinked';
+	public static Expanded = 'expanded';
+}
+
 @Component({
 	selector: 'yield-manager-dashboard',
 	templateUrl: '/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/template/yield-manager-dashboard.html',
-	directives: [CustomScroll, LazyLoadingTableComponent, YieldFilterPaneComponent, YieldKeyMetricsComponent, YieldPriceProductsComponent],
+	directives: [CustomScroll, LazyLoadingTableComponent, YieldFilterPaneComponent, YieldKeyMetricsComponent, YieldViewModeComponent, YieldPriceProductsComponent],
 	providers: [YieldFiltersService,
 		YieldManagerDashboardFilterService, YieldManagerDashboardPriceProductsService, YieldManagerDashboardKeyMetricsService]
 })
 export class YieldManagerDashboardComponent extends AHomeContainerComponent implements OnInit, IYieldManagerDashboardFilter, IYieldManagerDashboardPriceProducts {
+	public keyMetricsViewModeDecoratorClass =  ViewModeDecoratorClass.Default;
+	public priceProductsViewModeDecoratorClass =  ViewModeDecoratorClass.Default;
 
 	constructor(headerPageService: HeaderPageService) {
 		super(headerPageService, HeaderPageType.YieldManager);
 	}
 
 	ngOnInit() {
+	}
+
+	public viewModeStateChanged(state:YieldViewModeState){
+		switch(state){
+			case YieldViewModeState.Default:{
+				this.keyMetricsViewModeDecoratorClass = ViewModeDecoratorClass.Default;
+				this.priceProductsViewModeDecoratorClass = ViewModeDecoratorClass.Default;
+				break;
+			}
+			case YieldViewModeState.ExpandedYieldKeyMetrics:{
+				this.keyMetricsViewModeDecoratorClass = ViewModeDecoratorClass.Expanded;
+				this.priceProductsViewModeDecoratorClass = ViewModeDecoratorClass.Shrinked;
+				break;
+			}
+			case YieldViewModeState.ExpandedYieldPriceProducts:{
+				this.keyMetricsViewModeDecoratorClass = ViewModeDecoratorClass.Shrinked;
+				this.priceProductsViewModeDecoratorClass = ViewModeDecoratorClass.Expanded;
+				break;
+			}
+		}
 	}
 }
---
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-view-mode/YieldViewModeComponent.ts
@@ -0,0 +1,99 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+export enum YieldViewModeState{
+	Default,
+	ExpandedYieldKeyMetrics,
+	ExpandedYieldPriceProducts
+}
+
+class ViewModeButton{
+	constructor(private _state: YieldViewModeState, private _active: boolean){
+	}
+
+	public get state() : YieldViewModeState {
+		return this._state;
+	}
+
+	public set state(v : YieldViewModeState) {
+		this._state = v;
+	}
+	
+	public get active() : boolean {
+		return this._active;
+	}
+	public set active(v : boolean) {
+		this._active = v;
+	}
+	
+	public getIcon():string{
+		switch(this._state){
+			case YieldViewModeState.Default : {
+				return '-';
+			}
+			case YieldViewModeState.ExpandedYieldKeyMetrics :{
+				return ',';
+			}
+			case YieldViewModeState.ExpandedYieldPriceProducts: {
+				return '/';
+			}
+		}
+	}
+}
+@Component({
+	selector: 'yield-view-mode',
+	templateUrl: 'client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-view-mode/template/yield-view-mode.html'
+})
+export class YieldViewModeComponent implements OnInit {
+	public mouseHover: boolean;
+	public buttonLeft: ViewModeButton;
+	public buttonMiddle: ViewModeButton;
+	public buttonRight: ViewModeButton;
+	
+	@Output() onStateChange = new EventEmitter<YieldViewModeState>();
+
+	constructor() { 
+		this.buttonLeft = new ViewModeButton(YieldViewModeState.ExpandedYieldKeyMetrics, false);
+		this.buttonMiddle = new ViewModeButton(YieldViewModeState.Default, false);
+		this.buttonRight = new ViewModeButton(YieldViewModeState.ExpandedYieldPriceProducts, false);
+	}
+
+	ngOnInit() { }
+
+	public changeState(button:ViewModeButton){
+		var oldState = this.buttonMiddle.state;
+		var newState = button.state;
+
+		if (newState != oldState){
+			switch(button.state){
+				case YieldViewModeState.Default : {
+					this.buttonLeft.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+					this.buttonMiddle.state = YieldViewModeState.Default;
+					this.buttonRight.state = YieldViewModeState.ExpandedYieldPriceProducts;
+					break;
+				}
+				case YieldViewModeState.ExpandedYieldKeyMetrics :{
+					this.buttonLeft.state = YieldViewModeState.ExpandedYieldPriceProducts;
+					this.buttonMiddle.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+					this.buttonRight.state = YieldViewModeState.Default;
+					break;
+				}
+				case YieldViewModeState.ExpandedYieldPriceProducts: {
+					this.buttonLeft.state = YieldViewModeState.Default;
+					this.buttonMiddle.state = YieldViewModeState.ExpandedYieldPriceProducts;
+					this.buttonRight.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+					break;
+				}
+			}
+
+			this.onStateChange.emit(newState);
+		}
+	}
+
+	public mouseOver(){
+		this.mouseHover = true;
+	}
+
+	public mouseLeave(){
+		this.mouseHover = false;
+	}
+}
