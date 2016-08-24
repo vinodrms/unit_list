fc29f7ff3af19831539c66f6feb5701ed4b0e7ff
Dobre Catalin Andrei <catalin.dobre@3angletech.com>
2016-08-25 00:53:37 +0300

    [T258] Implemented Change date

--- assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-timeframe-header/YieldTimeFrameHeaderComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-timeframe-header/YieldTimeFrameHeaderComponent.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, AfterViewInit, ViewChildren, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChildren, Input, Output, ElementRef, Inject, EventEmitter } from '@angular/core';
 
 import {ThDateDO} from '../../../../../../../../services/common/data-objects/th-dates/ThDateDO';
 import {AppContext} from '../../../../../../../../../../common/utils/AppContext';
@@ -9,6 +9,8 @@ import {IYieldManagerDashboardFilter} from '../../YieldManagerDashboardComponent
 import {HotelService} from '../../../../../../../../services/hotel/HotelService';
 import {HotelDetailsDO} from '../../../../../../../../services/hotel/data-objects/HotelDetailsDO';
 
+import {ThDatePickerComponent} from '../../../../../../../../../../common/utils/components/ThDatePickerComponent';
+
 interface ITimeFrameOption{
 	noDays: number,
 	label: string,
@@ -17,9 +19,10 @@ interface ITimeFrameOption{
 
 @Component({
 	selector: 'yield-timeframe-header',
-	templateUrl: 'client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-timeframe-header/template/yield-timeframe-header.html'
+	templateUrl: 'client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-timeframe-header/template/yield-timeframe-header.html',
+	directives: [ThDatePickerComponent]
 })
-export class YieldTimeFrameHeaderComponent implements OnInit {
+export class YieldTimeFrameHeaderComponent implements OnInit,AfterViewInit {
 	public selectedDate: ThDateDO;
 	public selectedTimeFrame: ITimeFrameOption;
 	private _yieldManager: IYieldManagerDashboardFilter;
@@ -28,7 +31,8 @@ export class YieldTimeFrameHeaderComponent implements OnInit {
 
 	constructor(
 		private _hotelService: HotelService,
-		private _appContext: AppContext) {
+		private _appContext: AppContext,
+		@Inject(ElementRef) private _elementRef: ElementRef) {
 			this.timeFrameOptionsList = [
 				{
 					noDays: 7,
@@ -56,6 +60,30 @@ export class YieldTimeFrameHeaderComponent implements OnInit {
 			}, (error:any) => {
 				this._appContext.toaster.error(error.message);
 			});
+	}
+
+	ngAfterViewInit(){
+		var options: Object = {
+			singleDatePicker: true,
+			showDropdowns: true
+		};
+		var jQueryElement: any = this.getJQueryElement();
+		jQueryElement.daterangepicker(options, (start: moment.Moment, end: moment.Moment, label) => {
+			this.didSelectMoment(start);
+		});
+	}
+
+	private didSelectMoment(dateMoment: moment.Moment) {
+		var newThDate = new ThDateDO();
+		newThDate.day = dateMoment.date();
+		newThDate.month = dateMoment.month();
+		newThDate.year = dateMoment.year();
+		this.selectedDate = newThDate;
+		this.refresh();
+	}	
+
+	private getJQueryElement(): any {
+		return $(this._elementRef.nativeElement).find(".btn-change-date");
 	}
 
 	public get yieldManager(): IYieldManagerDashboardFilter {
