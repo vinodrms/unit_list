2de504deeb73ef99ee0dd10b64f3d26e859fe323
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-08-26 19:44:45 +0300

    YM: save view mode state in cookie

--- assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-view-mode/YieldViewModeComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-view-mode/YieldViewModeComponent.ts
@@ -1,36 +1,37 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {AppContext} from '../../../../../../../../../../common/utils/AppContext';
 
-export enum YieldViewModeState{
+export enum YieldViewModeState {
 	Default,
 	ExpandedYieldKeyMetrics,
 	ExpandedYieldPriceProducts
 }
 
-class ViewModeButton{
-	constructor(private _state: YieldViewModeState, private _active: boolean){
+class ViewModeButton {
+	constructor(private _state: YieldViewModeState, private _active: boolean) {
 	}
 
-	public get state() : YieldViewModeState {
+	public get state(): YieldViewModeState {
 		return this._state;
 	}
 
-	public set state(v : YieldViewModeState) {
+	public set state(v: YieldViewModeState) {
 		this._state = v;
 	}
-	
-	public get active() : boolean {
+
+	public get active(): boolean {
 		return this._active;
 	}
-	public set active(v : boolean) {
+	public set active(v: boolean) {
 		this._active = v;
 	}
-	
-	public getIcon():string{
-		switch(this._state){
-			case YieldViewModeState.Default : {
+
+	public getIcon(): string {
+		switch (this._state) {
+			case YieldViewModeState.Default: {
 				return 'a';
 			}
-			case YieldViewModeState.ExpandedYieldKeyMetrics :{
+			case YieldViewModeState.ExpandedYieldKeyMetrics: {
 				return 'c';
 			}
 			case YieldViewModeState.ExpandedYieldPriceProducts: {
@@ -45,58 +46,74 @@ class ViewModeButton{
 	templateUrl: 'client/src/pages/internal/containers/home/pages/home-pages/yield-manager/dashboard/components/yield-view-mode/template/yield-view-mode.html'
 })
 export class YieldViewModeComponent implements OnInit {
+	private static YMViewModeStateCookieKey: string = "YMViewModeStateKey";
 	public mouseHover: boolean;
 	public buttonLeft: ViewModeButton;
 	public buttonMiddle: ViewModeButton;
 	public buttonRight: ViewModeButton;
-	
+
 	@Output() onStateChange = new EventEmitter<YieldViewModeState>();
 
-	constructor() { 
+	constructor(private _appContext: AppContext) {
 		this.buttonLeft = new ViewModeButton(YieldViewModeState.ExpandedYieldKeyMetrics, false);
 		this.buttonMiddle = new ViewModeButton(YieldViewModeState.Default, false);
 		this.buttonRight = new ViewModeButton(YieldViewModeState.ExpandedYieldPriceProducts, false);
 	}
 
-	ngOnInit() { 
-		this.changeState(this.buttonRight);
+	ngOnInit() {
+		var defaultVMState = this.getDefaultYieldViewModeState();
+		this.changeStateCore(defaultVMState);
 	}
 
-	public changeState(button:ViewModeButton){
+	private getDefaultYieldViewModeState(): YieldViewModeState {
+		var viewModeStateString: string = this._appContext.thCookie.getCookie(YieldViewModeComponent.YMViewModeStateCookieKey);
+		if (!this._appContext.thUtils.isUndefinedOrNull(viewModeStateString)) {
+			var viewModeState = parseInt(viewModeStateString);
+			if (_.isNumber(viewModeState)) {
+				return viewModeState;
+			}
+		}
+		return YieldViewModeState.Default;
+	}
+
+	public changeState(button: ViewModeButton) {
 		var oldState = this.buttonMiddle.state;
 		var newState = button.state;
-
-		if (newState != oldState){
-			switch(button.state){
-				case YieldViewModeState.Default : {
-					this.buttonLeft.state = YieldViewModeState.ExpandedYieldKeyMetrics;
-					this.buttonMiddle.state = YieldViewModeState.Default;
-					this.buttonRight.state = YieldViewModeState.ExpandedYieldPriceProducts;
-					break;
-				}
-				case YieldViewModeState.ExpandedYieldKeyMetrics :{
-					this.buttonLeft.state = YieldViewModeState.ExpandedYieldPriceProducts;
-					this.buttonMiddle.state = YieldViewModeState.ExpandedYieldKeyMetrics;
-					this.buttonRight.state = YieldViewModeState.Default;
-					break;
-				}
-				case YieldViewModeState.ExpandedYieldPriceProducts: {
-					this.buttonLeft.state = YieldViewModeState.Default;
-					this.buttonMiddle.state = YieldViewModeState.ExpandedYieldPriceProducts;
-					this.buttonRight.state = YieldViewModeState.ExpandedYieldKeyMetrics;
-					break;
-				}
-			}
-
-			this.onStateChange.emit(newState);
+		this.changeStateCore(newState, oldState);
+	}
+	private changeStateCore(newState: YieldViewModeState, oldState?: YieldViewModeState) {
+		if (newState === oldState) {
+			return;
 		}
+		switch (newState) {
+			case YieldViewModeState.Default: {
+				this.buttonLeft.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+				this.buttonMiddle.state = YieldViewModeState.Default;
+				this.buttonRight.state = YieldViewModeState.ExpandedYieldPriceProducts;
+				break;
+			}
+			case YieldViewModeState.ExpandedYieldKeyMetrics: {
+				this.buttonLeft.state = YieldViewModeState.ExpandedYieldPriceProducts;
+				this.buttonMiddle.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+				this.buttonRight.state = YieldViewModeState.Default;
+				break;
+			}
+			case YieldViewModeState.ExpandedYieldPriceProducts: {
+				this.buttonLeft.state = YieldViewModeState.Default;
+				this.buttonMiddle.state = YieldViewModeState.ExpandedYieldPriceProducts;
+				this.buttonRight.state = YieldViewModeState.ExpandedYieldKeyMetrics;
+				break;
+			}
+		}
+		this.onStateChange.emit(newState);
+		this._appContext.thCookie.setCookie(YieldViewModeComponent.YMViewModeStateCookieKey, newState + "");
 	}
 
-	public mouseOver(){
+	public mouseOver() {
 		this.mouseHover = true;
 	}
 
-	public mouseLeave(){
+	public mouseLeave() {
 		this.mouseHover = false;
 	}
 }
