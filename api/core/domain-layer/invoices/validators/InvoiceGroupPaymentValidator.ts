d5241953889cb8f739d1ac32567c8be264da099f
Dragos Pricope <dragos.pricope@3angle.tech>
2016-08-31 19:28:14 +0300

    [T264] fixed issue when error is thrown in InvoicesGroupsRepo

--- api/core/domain-layer/invoices/validators/InvoiceGroupPaymentValidator.ts
+++ api/core/domain-layer/invoices/validators/InvoiceGroupPaymentValidator.ts
@@ -2,34 +2,35 @@ import {ThLogger, ThLogLevel} from '../../../utils/logging/ThLogger';
 import {ThError} from '../../../utils/th-responses/ThError';
 import {ThStatusCode} from '../../../utils/th-responses/ThResponse';
 import {ThUtils} from '../../../utils/ThUtils';
-import {InvoiceDO} from '../../../data-layer/invoices/data-objects/InvoiceDO';
+import {InvoiceGroupDO} from '../../../data-layer/invoices/data-objects/InvoiceGroupDO';
 import {AddOnProductsContainer} from '../../../domain-layer/add-on-products/validators/results/AddOnProductsContainer';
 import {CustomersContainer} from '../../../domain-layer/customers/validators/results/CustomersContainer';
 import {PriceProductsContainer} from '../../../domain-layer/price-products/validators/results/PriceProductsContainer';
 
 import _ = require('underscore');
 
-export class InvoicePaymentValidator {
+//TODO Implement invoice group payment validator
+export class InvoiceGroupPaymentValidator {
     private _thUtils: ThUtils;
-    private _invoice: InvoiceDO;
+    private _invoiceGroup: InvoiceGroupDO;
 
     constructor(private _customersContainer: CustomersContainer, private _aopsContainer: AddOnProductsContainer, private _ppsContainer: PriceProductsContainer) {
         this._thUtils = new ThUtils();
     }
 
-    public validate(invoice: InvoiceDO): Promise<InvoiceDO> {
-        return new Promise<InvoiceDO>((resolve: { (result: InvoiceDO): void }, reject: { (err: ThError): void }) => {
+    public validate(invoice: InvoiceGroupDO): Promise<InvoiceGroupDO> {
+        return new Promise<InvoiceGroupDO>((resolve: { (result: InvoiceGroupDO): void }, reject: { (err: ThError): void }) => {
             try {
                 this.validateCore(resolve, reject);
             } catch (error) {
                 var thError = new ThError(ThStatusCode.InvoicePaymentValidatorError, error);
-                ThLogger.getInstance().logError(ThLogLevel.Error, "error validating payment", this._invoice, thError);
+                ThLogger.getInstance().logError(ThLogLevel.Error, "error validating payment", this._invoiceGroup, thError);
                 reject(thError);
             }
         });
     }
 
-    private validateCore(resolve: { (result: InvoiceDO): void }, reject: { (err: ThError): void }) {
+    private validateCore(resolve: { (result: InvoiceGroupDO): void }, reject: { (err: ThError): void }) {
         
     }
 
