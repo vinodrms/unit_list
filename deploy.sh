0d6d8fac8b891455ebacd26e8ba72e054826f801
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-09-06 19:34:56 +0300

    decommented rimraf from azure deploy script

--- deploy.sh
+++ deploy.sh
@@ -122,17 +122,17 @@ selectNodeVersion
 # cp -R ../native_modules/* ./node_modules/
 
 # 3.0 install rimraf
-eval $NPM_CMD install -g rimraf
+# eval $NPM_CMD install -g rimraf
 
 # 3. Install server npm packages
 cd "$DEPLOYMENT_TARGET"
-eval rimraf node_modules
+# eval rimraf node_modules
 eval $NPM_CMD install
 exitWithMessageOnError "npm failed"
 
 # 4. Install client npm packages
 cd "assets"
-eval rimraf node_modules
+# eval rimraf node_modules
 eval $NPM_CMD install
 exitWithMessageOnError "npm failed"
 cd ".."
