e2a86c37393bf159de7582acd4d0e862874ec9c3
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-09-15 20:12:04 +0300

    added rimraf deletion of client node_modules

--- deploy.sh
+++ deploy.sh
@@ -110,11 +110,11 @@ fi
 selectNodeVersion
 
 # 2.1 Install rimraf and remove node_module for client and server
-# eval $NPM_CMD install -g rimraf
+eval $NPM_CMD install -g rimraf
 # cd "$DEPLOYMENT_TARGET"
 # rimraf node_modules
-# cd "assets"
-# rimraf node_modules
+cd "assets"
+rimraf node_modules
 
 # 2.2. Copy native Node modules
 # cd "$DEPLOYMENT_TARGET"
