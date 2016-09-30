44445b5480cafeceff95d7b72ccd5899f6e20290
Ionut Paraschiv <ionut.paraschiv@3angle.tech>
2016-09-30 15:03:30 +0300

    #27 Added additional messages on Price Product change

--- assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/operations-modal/components/components/booking-operations/components/price-product-view/components/change-price-product/ChangePriceProductModalComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/operations-modal/components/components/booking-operations/components/price-product-view/components/change-price-product/ChangePriceProductModalComponent.ts
@@ -40,11 +40,56 @@ export class ChangePriceProductModalComponent implements ICustomModalComponent, 
 
     public selectBookingCartItem(bookingCartItemVM: BookingCartItemVM) {
         this.selectedBookingCartItemVM = bookingCartItemVM;
-        console.log(bookingCartItemVM);
     }
 
     public useSelectedBookingCartItem() {
-        if (!this.didSelectBookingCartItem) { return; }
-        // TODO run the change price product rest api and return the updated booking to the dialog ref
+        if (!this.didSelectBookingCartItem || this.isUpdatingPriceProduct) { return; }
+        var booking = this.bookingSearchInput.booking;
+        var transientBookingItem = this.selectedBookingCartItemVM.transientBookingItem;
+
+        if (!this.needsConfirmation()) {
+            this.changeBookingPriceProduct();
+            return;
+        }
+        var confirmationDetails = this.getConfirmationDetails();
+        var title = this._appContext.thTranslation.translate(confirmationDetails.title);
+        var content = this._appContext.thTranslation.translate(confirmationDetails.message);
+        this._appContext.modalService.confirm(title, content, { positive: this._appContext.thTranslation.translate("Yes"), negative: this._appContext.thTranslation.translate("No") },
+            () => {
+                this.changeBookingPriceProduct();
+            }, () => { });
+    }
+
+    private needsConfirmation(): boolean {
+        var booking = this.bookingSearchInput.booking;
+        var transientBookingItem = this.selectedBookingCartItemVM.transientBookingItem;
+        return booking.roomCategoryId !== transientBookingItem.roomCategoryId || booking.priceProductId !== transientBookingItem.priceProductId;
+    }
+    private getConfirmationDetails(): { title: string, message: string } {
+        var booking = this.bookingSearchInput.booking;
+        var transientBookingItem = this.selectedBookingCartItemVM.transientBookingItem;
+
+        if (booking.roomCategoryId !== transientBookingItem.roomCategoryId && booking.priceProductId !== transientBookingItem.priceProductId) {
+            return {
+                title: "Change Price Product and Room Category",
+                message: "Are you sure you want to change the Price Product and Room Category from this booking? This will affect the total price."
+            }
+        }
+        if (booking.priceProductId !== transientBookingItem.priceProductId) {
+            return {
+                title: "Change Price Product",
+                message: "Are you sure you want to change the Price Product from this booking? This will affect the total price."
+            }
+        }
+        return {
+            title: "Change Room Category",
+            message: "Are you sure you want to change the Room Category from this booking? This will affect the total price."
+        }
+    }
+
+
+    private changeBookingPriceProduct() {
+        console.log(this.selectedBookingCartItemVM);
+        // TODO: run change price product REST Api
     }
 }
