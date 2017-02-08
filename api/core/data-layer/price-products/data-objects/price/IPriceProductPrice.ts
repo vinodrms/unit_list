00b26cc5106ba61dab4d5d1aaa585fd7dbe6e992
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-08 16:47:47 +0200

    #117 Included the room category stats list when checking the price configuration on the price product

--- api/core/data-layer/price-products/data-objects/price/IPriceProductPrice.ts
+++ api/core/data-layer/price-products/data-objects/price/IPriceProductPrice.ts
@@ -1,7 +1,7 @@
-import {BaseDO} from '../../../common/base/BaseDO';
-import {RoomCategoryStatsDO} from '../../../room-categories/data-objects/RoomCategoryStatsDO';
-import {ConfigCapacityDO} from '../../../common/data-objects/bed-config/ConfigCapacityDO';
-import {IndexedBookingInterval} from '../../utils/IndexedBookingInterval';
+import { BaseDO } from '../../../common/base/BaseDO';
+import { RoomCategoryStatsDO } from '../../../room-categories/data-objects/RoomCategoryStatsDO';
+import { ConfigCapacityDO } from '../../../common/data-objects/bed-config/ConfigCapacityDO';
+import { IndexedBookingInterval } from '../../utils/IndexedBookingInterval';
 
 export enum PriceProductPriceType {
 	SinglePrice,
@@ -16,6 +16,7 @@ export enum PriceProductPriceConfigurationState {
 export interface PriceProductPriceQueryDO {
 	roomCategoryId: string;
 	configCapacity: ConfigCapacityDO;
+	roomCategoryStatsList: RoomCategoryStatsDO[];
 }
 
 export interface IPriceProductPrice extends BaseDO {
