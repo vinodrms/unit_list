96b1f0608c39bfa9dbb9d836ffecc6ad88daa933
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-08 12:41:35 +0200

    #117 Added the bed stats on the room category stats

--- api/core/domain-layer/room-categories/aggregators/RoomCategoryStatsAggregator.ts
+++ api/core/domain-layer/room-categories/aggregators/RoomCategoryStatsAggregator.ts
@@ -1,19 +1,20 @@
-import {ThLogger, ThLogLevel} from '../../../utils/logging/ThLogger';
-import {ThError} from '../../../utils/th-responses/ThError';
-import {ThUtils} from '../../../utils/ThUtils';
-import {ThStatusCode} from '../../../utils/th-responses/ThResponse';
-import {AppContext} from '../../../utils/AppContext';
-import {SessionContext} from '../../../utils/SessionContext';
-import {RoomDO} from '../../../data-layer/rooms/data-objects/RoomDO';
-import {RoomCategoryDO} from '../../../data-layer/room-categories/data-objects/RoomCategoryDO';
-import {BedConfigDO} from '../../../data-layer/room-categories/data-objects/bed-config/BedConfigDO';
-import {BedMetaDO} from '../../../data-layer/room-categories/data-objects/bed-config/BedMetaDO';
-import {BedDO, BedStorageType, BedAccommodationType} from '../../../data-layer/common/data-objects/bed/BedDO';
-import {RoomCategorySearchResultRepoDO} from '../../../data-layer/room-categories/repositories/IRoomCategoryRepository';
-import {ConfigCapacityDO} from '../../../data-layer/common/data-objects/bed-config/ConfigCapacityDO';
-import {RoomCategoryStatsDO, RoomCategoryCapacityDO} from '../../../data-layer/room-categories/data-objects/RoomCategoryStatsDO';
-import {RoomMetaRepoDO, RoomSearchResultRepoDO} from '../../../data-layer/rooms/repositories/IRoomRepository';
-import {BedSearchResultRepoDO} from '../../../data-layer/beds/repositories/IBedRepository';
+import { ThLogger, ThLogLevel } from '../../../utils/logging/ThLogger';
+import { ThError } from '../../../utils/th-responses/ThError';
+import { ThUtils } from '../../../utils/ThUtils';
+import { ThStatusCode } from '../../../utils/th-responses/ThResponse';
+import { AppContext } from '../../../utils/AppContext';
+import { SessionContext } from '../../../utils/SessionContext';
+import { RoomDO } from '../../../data-layer/rooms/data-objects/RoomDO';
+import { RoomCategoryDO } from '../../../data-layer/room-categories/data-objects/RoomCategoryDO';
+import { BedConfigDO } from '../../../data-layer/room-categories/data-objects/bed-config/BedConfigDO';
+import { BedMetaDO } from '../../../data-layer/room-categories/data-objects/bed-config/BedMetaDO';
+import { BedDO, BedStorageType, BedAccommodationType } from '../../../data-layer/common/data-objects/bed/BedDO';
+import { RoomCategorySearchResultRepoDO } from '../../../data-layer/room-categories/repositories/IRoomCategoryRepository';
+import { ConfigCapacityDO } from '../../../data-layer/common/data-objects/bed-config/ConfigCapacityDO';
+import { RoomCategoryStatsDO, RoomCategoryCapacityDO } from '../../../data-layer/room-categories/data-objects/RoomCategoryStatsDO';
+import { BedStatsDO } from '../../../data-layer/room-categories/data-objects/bed-stats/BedStatsDO';
+import { RoomMetaRepoDO, RoomSearchResultRepoDO } from '../../../data-layer/rooms/repositories/IRoomRepository';
+import { BedSearchResultRepoDO } from '../../../data-layer/beds/repositories/IBedRepository';
 
 import _ = require('underscore');
 
@@ -23,6 +24,7 @@ export class RoomCategoryStatsAggregator {
     constructor(private _appContext: AppContext, private _sessionContext: SessionContext) {
         this._thUtils = new ThUtils();
     }
+
     public getUsedRoomCategoryList(): Promise<RoomCategoryDO[]> {
         return new Promise<RoomCategoryDO[]>((resolve: { (result: RoomCategoryDO[]): void }, reject: { (err: ThError): void }) => {
             this.getUsedRoomCategoryListCore(resolve, reject);
@@ -44,7 +46,7 @@ export class RoomCategoryStatsAggregator {
             reject(thError);
         });
     }
-    
+
     public getUsedRoomCategoryStatsList(): Promise<RoomCategoryStatsDO[]> {
         return new Promise<RoomCategoryStatsDO[]>((resolve: { (result: RoomCategoryStatsDO[]): void }, reject: { (err: ThError): void }) => {
             this.getUsedRoomCategoryStatsListCore(resolve, reject);
@@ -57,17 +59,18 @@ export class RoomCategoryStatsAggregator {
         }).then((roomCategoryStatsList: RoomCategoryStatsDO[]) => {
             resolve(roomCategoryStatsList);
         }).catch((error: any) => {
-            reject(error);      
+            reject(error);
         });
     }
+
     public getRoomCategoryStatsList(roomCategoryIdList?: string[]): Promise<RoomCategoryStatsDO[]> {
         return new Promise<RoomCategoryStatsDO[]>((resolve: { (result: RoomCategoryStatsDO[]): void }, reject: { (err: ThError): void }) => {
             this.getRoomCategoryStatsListCore(resolve, reject, roomCategoryIdList);
         });
     }
     private getRoomCategoryStatsListCore(resolve: { (result: RoomCategoryStatsDO[]): void }, reject: { (err: ThError): void }, roomCategoryIdList: string[]) {
-        var distinctBedIdList = [];
-        var roomCategoryStatsList = [];
+        var distinctBedIdList: string[] = [];
+        var roomCategoryStatsList: RoomCategoryStatsDO[] = [];
 
         this.getRoomCategoryList(roomCategoryIdList).then((result: RoomCategorySearchResultRepoDO) => {
             distinctBedIdList = this.getDistinctBedIdListFromRoomCategoryList(result.roomCategoryList);
@@ -80,17 +83,19 @@ export class RoomCategoryStatsAggregator {
 
             return this.getRoomListByRoomCategoryIdList(roomCategoryIdList);
         }).then((roomList: RoomDO[]) => {
-            var noOfRoomStatsList = this.getNoOfRoomsStatsGroupedByCategoryId(roomList);
+            let indexedNoRoomsByCategId = this.getIndexedNoOfRoomsIndexedByCategoryId(roomList);
 
             _.forEach(roomCategoryStatsList, (roomCategoryStats: RoomCategoryStatsDO) => {
-                roomCategoryStats.noOfRooms = this.getNoOfRoomsByRoomCategory(roomCategoryStats.roomCategory, noOfRoomStatsList);
+                roomCategoryStats.noOfRooms = this.getNoOfRoomsByRoomCategory(roomCategoryStats.roomCategory, indexedNoRoomsByCategId);
             });
 
             var bedRepo = this._appContext.getRepositoryFactory().getBedRepository();
             return bedRepo.getBedList({ hotelId: this.hotelId }, { bedIdList: distinctBedIdList });
         }).then((result: BedSearchResultRepoDO) => {
             _.forEach(roomCategoryStatsList, (roomCategoryStats: RoomCategoryStatsDO) => {
-                roomCategoryStats.capacity = this.getRoomCategoryBedConfigCapacity(roomCategoryStats.roomCategory, result.bedList);
+                let bedConfigDetails = this.getRoomCategoryBedConfigDetails(roomCategoryStats.roomCategory, result.bedList);
+                roomCategoryStats.capacity = bedConfigDetails.roomCategCapacity;
+                roomCategoryStats.bedStatsList = bedConfigDetails.bedStatsList;
             });
 
             resolve(roomCategoryStatsList);
@@ -109,39 +114,44 @@ export class RoomCategoryStatsAggregator {
             return bedMeta.bedId;
         }).flatten().uniq().value();
     }
-    private getNoOfRoomsByRoomCategory(roomCategory: RoomCategoryDO, noOfRoomsStatsList: any[]): number {
-        var noOfRoomsStats = _.find(noOfRoomsStatsList, ((noOfRoomsStats: any) => {
-            return noOfRoomsStats.categoryId === roomCategory.id;
-        }));
-        if (!this._thUtils.isUndefinedOrNull(noOfRoomsStats)) {
-            return noOfRoomsStats.noOfRooms;
+    private getNoOfRoomsByRoomCategory(roomCategory: RoomCategoryDO, indexedNoRoomsByCategId: { [index: string]: number; }): number {
+        let noOfRooms = indexedNoRoomsByCategId[roomCategory.id];
+        if (!this._thUtils.isUndefinedOrNull(noOfRooms)) {
+            return noOfRooms;
         }
         return 0;
     }
-    private getNoOfRoomsStatsGroupedByCategoryId(roomList: RoomDO[]): any[] {
-        return _.chain(roomList)
-            .groupBy('categoryId')
-            .map(function (value, key) {
-                return {
-                    categoryId: key,
-                    noOfRooms: value.length
-                }
-            }).value();
+    private getIndexedNoOfRoomsIndexedByCategoryId(roomList: RoomDO[]): { [index: string]: number; } {
+        let indexedNoRoomsByCategId: { [index: string]: number; } = {};
+        _.chain(roomList).groupBy('categoryId').forEach((value: RoomDO[], key: string) => {
+            indexedNoRoomsByCategId[key] = value.length;
+        });
+        return indexedNoRoomsByCategId;
     }
-    private getRoomCategoryBedConfigCapacity(roomCategory: RoomCategoryDO, allBeds: BedDO[]): RoomCategoryCapacityDO {
+    private getRoomCategoryBedConfigDetails(roomCategory: RoomCategoryDO, allBeds: BedDO[]): { roomCategCapacity: RoomCategoryCapacityDO, bedStatsList: BedStatsDO[] } {
         var capacity: RoomCategoryCapacityDO;
-        var bedList = this.getBedListFromRommCategory(roomCategory, allBeds);
+        var bedList = this.filterBedListForRoomCategory(roomCategory, allBeds);
+        var bedStatsList: BedStatsDO[] = [];
         if (!_.isEmpty(bedList)) {
             capacity = new RoomCategoryCapacityDO();
-            capacity.rollawayCapacity = this.getBedConfigCapacityByStorageType(roomCategory.bedConfig, bedList, BedStorageType.Rollaway);
-            capacity.stationaryCapacity = this.getBedConfigCapacityByStorageType(roomCategory.bedConfig, bedList, BedStorageType.Stationary);
+
+            let stationaryBedDetails = this.getBedDetailsByStorageType(roomCategory.bedConfig, bedList, BedStorageType.Stationary);
+            capacity.stationaryCapacity = stationaryBedDetails.capacity;
+            bedStatsList = bedStatsList.concat(stationaryBedDetails.bedStatsList);
+
+            let rollawayBedDetais = this.getBedDetailsByStorageType(roomCategory.bedConfig, bedList, BedStorageType.Rollaway);
+            capacity.rollawayCapacity = rollawayBedDetais.capacity;
+            bedStatsList = bedStatsList.concat(rollawayBedDetais.bedStatsList);
         }
         else {
             capacity = this.getZeroCapacityCategoryStats();
         }
-        return capacity;
+        return {
+            roomCategCapacity: capacity,
+            bedStatsList: bedStatsList
+        }
     }
-    private getBedListFromRommCategory(roomCategory: RoomCategoryDO, allBeds: BedDO[]): BedDO[] {
+    private filterBedListForRoomCategory(roomCategory: RoomCategoryDO, allBeds: BedDO[]): BedDO[] {
         var bedIdList = _.map(roomCategory.bedConfig.bedMetaList, (bedMeta: BedMetaDO) => {
             return bedMeta.bedId;
         });
@@ -166,84 +176,84 @@ export class RoomCategoryStatsAggregator {
             reject(thError);
         });
     }
-    private getRoomCategoryList(roomCategoryIdList?: string[]) {
-        return new Promise<RoomCategorySearchResultRepoDO>((resolve: { (result: RoomCategorySearchResultRepoDO): void }, reject: { (err: ThError): void }) => {
-            this.getRoomCategoryListCore(resolve, reject, roomCategoryIdList);
-        });
-    }
-    private getRoomCategoryListCore(resolve: { (result: RoomCategorySearchResultRepoDO): void }, reject: { (err: ThError): void }, roomCategoryIdList?: string[]) {
-        var roomRepository = this._appContext.getRepositoryFactory().getRoomRepository();
-        var roomCategoryRepository = this._appContext.getRepositoryFactory().getRoomCategoryRepository();
-
-        var roomCategoriesPromise: Promise<RoomCategorySearchResultRepoDO>;
-
+    private getRoomCategoryList(roomCategoryIdList?: string[]): Promise<RoomCategorySearchResultRepoDO> {
+        let roomCategoryRepository = this._appContext.getRepositoryFactory().getRoomCategoryRepository();
         if (this._thUtils.isUndefinedOrNull(roomCategoryIdList)) {
-            roomCategoriesPromise = roomCategoryRepository.getRoomCategoryList({ hotelId: this.hotelId });
+            return roomCategoryRepository.getRoomCategoryList({ hotelId: this.hotelId });
         }
-        else {
-            roomCategoriesPromise = roomCategoryRepository.getRoomCategoryList({ hotelId: this.hotelId }, { categoryIdList: roomCategoryIdList });
-        }
-
-        roomCategoriesPromise.then((result: RoomCategorySearchResultRepoDO) => {
-            resolve(result);
-        }).catch((error: any) => {
-            var thError = new ThError(ThStatusCode.RoomAggregatorCategoryStatsListError, error);
-            if (thError.isNativeError()) {
-                ThLogger.getInstance().logError(ThLogLevel.Error, "error getting room category list", null, thError);
-            }
-            reject(thError);
-        });
+        return roomCategoryRepository.getRoomCategoryList({ hotelId: this.hotelId }, { categoryIdList: roomCategoryIdList });
     }
-    private getBedConfigCapacityByStorageType(bedConfig: BedConfigDO, bedList: BedDO[], bedStorageType: BedStorageType): ConfigCapacityDO {
+
+    private getBedDetailsByStorageType(bedConfig: BedConfigDO, bedList: BedDO[], bedStorageType: BedStorageType): { capacity: ConfigCapacityDO, bedStatsList: BedStatsDO[] } {
         var configCapacity = new ConfigCapacityDO();
         configCapacity.noBabies = 0;
         configCapacity.noAdults = 0;
         configCapacity.noChildren = 0;
 
         var bedListToAggregate: BedDO[] = this.filterBedListByStorageType(bedList, bedStorageType);
+        var bedStatsList: BedStatsDO[] = [];
 
         _.forEach(bedListToAggregate, (bed: BedDO) => {
             var bedMeta = _.findWhere(bedConfig.bedMetaList, { bedId: bed.id });
 
+            let bedCapacity = this.getZeroCapacity();
             if (bed.accommodationType === BedAccommodationType.Babies) {
                 configCapacity.noBabies += bedMeta.noOfInstances;
+                bedCapacity.noBabies = 1;
             }
             else {
                 configCapacity.noAdults += bedMeta.noOfInstances * bed.capacity.maxNoAdults;
                 configCapacity.noChildren += bedMeta.noOfInstances * bed.capacity.maxNoChildren;
+                bedCapacity.noAdults = bed.capacity.maxNoAdults;
+                bedCapacity.noChildren = bed.capacity.maxNoChildren;
             }
+            bedStatsList = bedStatsList.concat(this.buildBedStatsList(bedCapacity, bedMeta.noOfInstances, bed));
         });
-
-        return configCapacity;
+        return {
+            capacity: configCapacity,
+            bedStatsList: bedStatsList
+        };
     }
     private filterBedListByStorageType(bedList: BedDO[], bedStorageType: BedStorageType): BedDO[] {
         switch (bedStorageType) {
-            case BedStorageType.Rollaway: return this.getRollawayBeds(bedList);
-            case BedStorageType.Stationary: return this.getStationaryBeds(bedList);
+            case BedStorageType.Rollaway: return this.filterRollawayBeds(bedList);
+            case BedStorageType.Stationary: return this.filterStationaryBeds(bedList);
             default: return [];
         }
     }
-    private getStationaryBeds(bedList: BedDO[]): BedDO[] {
+    private filterStationaryBeds(bedList: BedDO[]): BedDO[] {
         return _.filter(bedList, (bed: BedDO) => {
             return bed.storageType === BedStorageType.Stationary;
         });
     }
-    private getRollawayBeds(bedList: BedDO[]): BedDO[] {
+    private filterRollawayBeds(bedList: BedDO[]): BedDO[] {
         return _.filter(bedList, (bed: BedDO) => {
             return bed.storageType === BedStorageType.Rollaway;
         });
     }
+    private buildBedStatsList(bedCapacity: ConfigCapacityDO, noBeds: number, bed: BedDO): BedStatsDO[] {
+        var bedStatsList: BedStatsDO[] = [];
+        for (var i = 0; i < noBeds; i++) {
+            let stats = new BedStatsDO();
+            stats.capacity = bedCapacity.buildPrototype();
+            stats.storageType = bed.storageType;
+            bedStatsList.push(stats);
+        }
+        return bedStatsList;
+    }
     private getZeroCapacityCategoryStats(): RoomCategoryCapacityDO {
         var zeroCapacity: RoomCategoryCapacityDO = new RoomCategoryCapacityDO();
+        var emptyCapacity = this.getZeroCapacity();
+        zeroCapacity.stationaryCapacity = emptyCapacity;
+        zeroCapacity.rollawayCapacity = emptyCapacity;
+        return zeroCapacity;
+    }
+    private getZeroCapacity(): ConfigCapacityDO {
         var emptyCapacity = new ConfigCapacityDO();
-
         emptyCapacity.noAdults = 0;
         emptyCapacity.noBabies = 0;
         emptyCapacity.noChildren = 0;
-        zeroCapacity.stationaryCapacity = emptyCapacity;
-        zeroCapacity.rollawayCapacity = emptyCapacity;
-
-        return zeroCapacity;
+        return emptyCapacity;
     }
     private get hotelId(): string {
         return this._sessionContext.sessionDO.hotel.id;
