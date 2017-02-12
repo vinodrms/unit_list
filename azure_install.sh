a3f6342e1c573e89ce75497c875130e562b2bd43
Dragos Pricope <dragos.pricope@3angle.tech>
2017-02-12 10:54:20 +0200

    #101 Fixed playbook's path in deployment script

--- azure_install.sh
+++ azure_install.sh
@@ -4,7 +4,7 @@ mkdir azure
 cd azure
 git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git --branch 101-configure-linux-vm-on-azure
 cd UnitPal
-ansible-playbook -i production --private-key=~/.ssh/unitpal_rsa -u unitpal unitpal_update.yml
+ansible-playbook -i production --private-key=~/.ssh/unitpal_rsa -u unitpal ansible/unitpal_update.yml
 cd ../../
 rm -rf azure
 echo "Finished install on Azure"
