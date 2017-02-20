b01005951e1cfc162d9c370baffde0cfe183d535
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-20 16:38:02 +0200

    #108 Added test for Undo Check In on a booking

--- api/test/root/unit-tests/hotel-operations/dashboard/helpers/HotelDashboardOperationsTestHelper.ts
+++ api/test/root/unit-tests/hotel-operations/dashboard/helpers/HotelDashboardOperationsTestHelper.ts
@@ -1,11 +1,11 @@
-import {DefaultDataBuilder} from '../../../../../db-initializers/DefaultDataBuilder';
-import {ThDateIntervalDO} from '../../../../../../core/utils/th-dates/data-objects/ThDateIntervalDO';
-import {ThTimestampDO} from '../../../../../../core/utils/th-dates/data-objects/ThTimestampDO';
-import {ThDateUtils} from '../../../../../../core/utils/th-dates/ThDateUtils';
-import {HotelOperationsQueryDO} from '../../../../../../core/domain-layer/hotel-operations/dashboard/utils/HotelOperationsQueryDO';
-import {AssignRoomDO} from '../../../../../../core/domain-layer/hotel-operations/room/assign/AssignRoomDO';
-import {BookingDO} from '../../../../../../core/data-layer/bookings/data-objects/BookingDO';
-import {RoomDO} from '../../../../../../core/data-layer/rooms/data-objects/RoomDO';
+import { DefaultDataBuilder } from '../../../../../db-initializers/DefaultDataBuilder';
+import { ThDateIntervalDO } from '../../../../../../core/utils/th-dates/data-objects/ThDateIntervalDO';
+import { ThTimestampDO } from '../../../../../../core/utils/th-dates/data-objects/ThTimestampDO';
+import { ThDateUtils } from '../../../../../../core/utils/th-dates/ThDateUtils';
+import { HotelOperationsQueryDO } from '../../../../../../core/domain-layer/hotel-operations/dashboard/utils/HotelOperationsQueryDO';
+import { AssignRoomDO } from '../../../../../../core/domain-layer/hotel-operations/room/assign/AssignRoomDO';
+import { BookingDO } from '../../../../../../core/data-layer/bookings/data-objects/BookingDO';
+import { RoomDO } from '../../../../../../core/data-layer/rooms/data-objects/RoomDO';
 
 import _ = require('underscore');
 
@@ -22,10 +22,10 @@ export class HotelDashboardOperationsTestHelper {
         var endDate = this._thDateUtils.addDaysToThDateDO(startDate.buildPrototype(), 1);
         return ThDateIntervalDO.buildThDateIntervalDO(startDate, endDate);
     }
-    public getFromTomorrowTwoDaysInterval(testDataBuilder: DefaultDataBuilder): ThDateIntervalDO {
+    public getFromTodayTwoDaysInterval(testDataBuilder: DefaultDataBuilder): ThDateIntervalDO {
         var interval = this.getTodayToTomorrowInterval(testDataBuilder);
-        interval.start = this._thDateUtils.addDaysToThDateDO(interval.start, 1);
-        interval.end = this._thDateUtils.addDaysToThDateDO(interval.end, 2);
+        interval.start = this._thDateUtils.addDaysToThDateDO(interval.start, 0);
+        interval.end = this._thDateUtils.addDaysToThDateDO(interval.end, 1);
         return interval;
     }
     public getQueryForToday(testDataBuilder: DefaultDataBuilder): HotelOperationsQueryDO {
