2fb8252ba4185146003c557533586f1ec9292f16
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-23 08:19:08 +0000

    #119 Add/Edit customer within the selection modal

--- assets/client/src/pages/internal/containers/common/inventory/customer-register/modal/CustomerRegisterModalModule.ts
+++ assets/client/src/pages/internal/containers/common/inventory/customer-register/modal/CustomerRegisterModalModule.ts
@@ -1,15 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {SharedPipesModule} from '../../../../../../../common/utils/pipes/modules/SharedPipesModule';
-import {SharedDirectivesModule} from '../../../../../../../common/utils/directives/modules/SharedDirectivesModule';
-import {SharedComponentsModule} from '../../../../../../../common/utils/components/modules/SharedComponentsModule';
+import { SharedPipesModule } from '../../../../../../../common/utils/pipes/modules/SharedPipesModule';
+import { SharedDirectivesModule } from '../../../../../../../common/utils/directives/modules/SharedDirectivesModule';
+import { SharedComponentsModule } from '../../../../../../../common/utils/components/modules/SharedComponentsModule';
+import { CommonCustRegisterModule } from '../CommonCustRegisterModule';
 
-import {CustomerRegisterModalComponent} from './CustomerRegisterModalComponent';
+import { CustomerRegisterModalComponent } from './CustomerRegisterModalComponent';
 
 @NgModule({
     imports: [CommonModule,
-        SharedPipesModule, SharedDirectivesModule, SharedComponentsModule],
+        SharedPipesModule, SharedDirectivesModule, SharedComponentsModule,
+        CommonCustRegisterModule],
     declarations: [CustomerRegisterModalComponent],
     exports: [CustomerRegisterModalComponent],
 })
