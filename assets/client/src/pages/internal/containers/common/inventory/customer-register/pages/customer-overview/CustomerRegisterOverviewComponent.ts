931a0865ac5fc9d592b6fea1bb9db095837752ce
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-23 06:56:09 +0000

    #119 Inject the HotelOperationsModalService programmatically from the CustomerRegisterOverviewComponent

--- assets/client/src/pages/internal/containers/common/inventory/customer-register/pages/customer-overview/CustomerRegisterOverviewComponent.ts
+++ assets/client/src/pages/internal/containers/common/inventory/customer-register/pages/customer-overview/CustomerRegisterOverviewComponent.ts
@@ -1,17 +1,16 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
-import {BaseComponent} from '../../../../../../../../common/base/BaseComponent';
-import {CustomerVM} from '../../../../../../services/customers/view-models/CustomerVM';
-import {CustomScroll} from '../../../../../../../../common/utils/directives/CustomScroll';
-import {ModalDialogRef} from '../../../../../../../../common/utils/modals/utils/ModalDialogRef';
-import {HotelOperationsModalService} from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/HotelOperationsModalService';
-import {HotelOperationsResult} from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/utils/HotelOperationsResult';
+import { Component, Input, Output, EventEmitter, ReflectiveInjector, Injector } from '@angular/core';
+import { BaseComponent } from '../../../../../../../../common/base/BaseComponent';
+import { AppContext } from '../../../../../../../../common/utils/AppContext';
+import { CustomerVM } from '../../../../../../services/customers/view-models/CustomerVM';
+import { CustomScroll } from '../../../../../../../../common/utils/directives/CustomScroll';
+import { ModalDialogRef } from '../../../../../../../../common/utils/modals/utils/ModalDialogRef';
+import { HotelOperationsModalService } from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/HotelOperationsModalService';
+import { HotelOperationsResult } from '../../../../../home/pages/home-pages/hotel-operations/operations-modal/services/utils/HotelOperationsResult';
 
 @Component({
 	selector: 'customer-register-overview',
-	templateUrl: '/client/src/pages/internal/containers/common/inventory/customer-register/pages/customer-overview/template/customer-register-overview.html',
-	providers: [HotelOperationsModalService]
+	templateUrl: '/client/src/pages/internal/containers/common/inventory/customer-register/pages/customer-overview/template/customer-register-overview.html'
 })
-
 export class CustomerRegisterOverviewComponent extends BaseComponent {
 	@Input() showLinkToOperationalModal: boolean = false;
 
@@ -29,12 +28,16 @@ export class CustomerRegisterOverviewComponent extends BaseComponent {
 		this.onEdit.next(this._customerVM);
 	}
 
-	constructor(private _hotelOperationsModalService: HotelOperationsModalService) {
+	constructor(private _appContext: AppContext,
+		private _injector: Injector) {
 		super();
 	}
 
 	public openCustomerOperationsModal() {
-		this._hotelOperationsModalService.openCustomerOperationsModal(this._customerVM.customer.id).then((modalDialogRef: ModalDialogRef<HotelOperationsResult>) => {
+		let resolvedProviders = ReflectiveInjector.resolve([HotelOperationsModalService]);
+		let injector = ReflectiveInjector.fromResolvedProviders(resolvedProviders, this._injector);
+		let hotelOperationsModalService: HotelOperationsModalService = injector.get(HotelOperationsModalService);
+		hotelOperationsModalService.openCustomerOperationsModal(this._customerVM.customer.id).then((modalDialogRef: ModalDialogRef<HotelOperationsResult>) => {
 			modalDialogRef.resultObservable
 				.subscribe((result: HotelOperationsResult) => {
 				}, (err: any) => {
