d505741205847325ae8f244cdda8aac0f7d23559
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-02-27 11:07:51 +0200

    #128 Implemented edit/add AOP functionality from the selection modal

--- assets/client/src/pages/internal/containers/common/inventory/add-on-products/modal/AddOnProductsModalModule.ts
+++ assets/client/src/pages/internal/containers/common/inventory/add-on-products/modal/AddOnProductsModalModule.ts
@@ -1,15 +1,17 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import {SharedPipesModule} from '../../../../../../../common/utils/pipes/modules/SharedPipesModule';
-import {SharedDirectivesModule} from '../../../../../../../common/utils/directives/modules/SharedDirectivesModule';
-import {SharedComponentsModule} from '../../../../../../../common/utils/components/modules/SharedComponentsModule';
+import { SharedPipesModule } from '../../../../../../../common/utils/pipes/modules/SharedPipesModule';
+import { SharedDirectivesModule } from '../../../../../../../common/utils/directives/modules/SharedDirectivesModule';
+import { SharedComponentsModule } from '../../../../../../../common/utils/components/modules/SharedComponentsModule';
+import { CommonAddOnProductsModule } from '../CommonAddOnProductsModule';
 
-import {AddOnProductsModalComponent} from './AddOnProductsModalComponent';
+import { AddOnProductsModalComponent } from './AddOnProductsModalComponent';
 
 @NgModule({
     imports: [CommonModule,
-        SharedPipesModule, SharedDirectivesModule, SharedComponentsModule],
+        SharedPipesModule, SharedDirectivesModule,
+        SharedComponentsModule, CommonAddOnProductsModule],
     declarations: [AddOnProductsModalComponent],
     exports: [AddOnProductsModalComponent],
 })
