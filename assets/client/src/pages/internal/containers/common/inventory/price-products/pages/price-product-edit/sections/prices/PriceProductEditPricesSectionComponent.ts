4323ebbe2f3c1917425c085b9abd185feed65783
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-03-02 19:18:23 +0200

    #175 Show pricing in box with border

--- assets/client/src/pages/internal/containers/common/inventory/price-products/pages/price-product-edit/sections/prices/PriceProductEditPricesSectionComponent.ts
+++ assets/client/src/pages/internal/containers/common/inventory/price-products/pages/price-product-edit/sections/prices/PriceProductEditPricesSectionComponent.ts
@@ -16,6 +16,12 @@ import { CurrencyDO } from '../../../../../../../../services/common/data-objects
 @Component({
 	selector: 'price-product-edit-prices-section',
 	templateUrl: '/client/src/pages/internal/containers/common/inventory/price-products/pages/price-product-edit/sections/prices/template/price-product-edit-prices-section.html',
+	styles: [
+		`	.price-product-box { outline: 1px solid #d3d3d3; outline-offset: 5px; margin-top: 5px; } 
+			.row-eq-height { display: flex; flex-wrap: wrap; }
+			.row-eq-height > [class*='col-'] { display: flex; flex-direction: column; }
+		`
+	],
 	providers: [RoomCategoriesStatsService]
 })
 export class PriceProductEditPricesSectionComponent extends BaseComponent implements IPriceProductEditSection {
@@ -105,7 +111,7 @@ export class PriceProductEditPricesSectionComponent extends BaseComponent implem
 	public displayError() {
 		return this.didSubmit || this.readonly;
 	}
-	
+
 	public getNoRoomsLabel(noOfRooms: number): string {
 		if (noOfRooms === 1) {
 			return this._appContext.thTranslation.translate("%noOfRooms% Room", { noOfRooms: noOfRooms });
