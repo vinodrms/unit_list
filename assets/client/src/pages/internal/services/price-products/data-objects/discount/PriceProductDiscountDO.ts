838a6fa236b2c1732192d077783dad9fb1962322
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-03-09 10:20:40 +0200

    #171 Added the discounts data objects on the client

---
+++ assets/client/src/pages/internal/services/price-products/data-objects/discount/PriceProductDiscountDO.ts
@@ -0,0 +1,19 @@
+import { BaseDO } from '../../../../../../common/base/BaseDO';
+import { PriceProductConstraintWrapperDO } from "../constraint/PriceProductConstraintWrapperDO";
+
+export class PriceProductDiscountDO extends BaseDO {
+    name: string;
+    value: number;
+    constraints: PriceProductConstraintWrapperDO;
+
+    protected getPrimitivePropertyKeys(): string[] {
+        return ["name", "value"];
+    }
+
+    public buildFromObject(object: Object) {
+        super.buildFromObject(object);
+
+        this.constraints = new PriceProductConstraintWrapperDO();
+        this.constraints.buildFromObject(this.getObjectPropertyEnsureUndefined(object, "constraints"));
+    }
+}
