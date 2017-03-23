9d4debaf912917481b241f1a605ca90dd0df2154
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-03-23 13:27:05 +0200

    #150 Enable angular prod mode when running on non development environment

--- assets/client/src/pages/external/main/ExternalBoot.ts
+++ assets/client/src/pages/external/main/ExternalBoot.ts
@@ -1,41 +1,41 @@
 /// <reference path="../../../../typings/tsd.d.ts" />
 
-import {NgModule, Compiler}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RouterModule} from "@angular/router";
-import {HttpModule} from "@angular/http";
+import { NgModule, Compiler, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
 
-import {MainLayoutExternalComponent} from './MainLayoutExternalComponent';
-import {ExternalRouting} from './ExternalRouterConfig';
-import {IThCookie} from '../../../common/utils/cookies/IThCookie';
-import {ThCookie} from '../../../common/utils/cookies/ThCookie';
-import {IThHttp} from '../../../common/utils/http/IThHttp';
-import {ThHttp} from '../../../common/utils/http/ThHttp';
-import {IBrowserLocation} from '../../../common/utils/browser-location/IBrowserLocation';
-import {BrowserLocation} from '../../../common/utils/browser-location/BrowserLocation';
-import {IRouterNavigator} from '../../../common/utils/navigator/IRouterNavigator';
-import {RouterNavigator} from '../../../common/utils/navigator/RouterNavigator';
-import {IToaster} from '../../../common/utils/toaster/IToaster';
-import {Toaster} from '../../../common/utils/toaster/Toaster';
-import {IModalService} from '../../../common/utils/modals/IModalService';
-import {ModalService} from '../../../common/utils/modals/ModalService';
-import {ThTranslation} from '../../../common/utils/localization/ThTranslation';
-import {GoogleAnalytics} from '../../../common/utils/analytics/GoogleAnalytics';
-import {IAnalytics} from '../../../common/utils/analytics/IAnalytics';
-import {AppContext} from '../../../common/utils/AppContext';
-import {ModuleLoaderService} from '../../../common/utils/module-loader/ModuleLoaderService';
+import { MainLayoutExternalComponent } from './MainLayoutExternalComponent';
+import { ExternalRouting } from './ExternalRouterConfig';
+import { IThCookie } from '../../../common/utils/cookies/IThCookie';
+import { ThCookie } from '../../../common/utils/cookies/ThCookie';
+import { IThHttp } from '../../../common/utils/http/IThHttp';
+import { ThHttp } from '../../../common/utils/http/ThHttp';
+import { IBrowserLocation } from '../../../common/utils/browser-location/IBrowserLocation';
+import { BrowserLocation } from '../../../common/utils/browser-location/BrowserLocation';
+import { IRouterNavigator } from '../../../common/utils/navigator/IRouterNavigator';
+import { RouterNavigator } from '../../../common/utils/navigator/RouterNavigator';
+import { IToaster } from '../../../common/utils/toaster/IToaster';
+import { Toaster } from '../../../common/utils/toaster/Toaster';
+import { IModalService } from '../../../common/utils/modals/IModalService';
+import { ModalService } from '../../../common/utils/modals/ModalService';
+import { ThTranslation } from '../../../common/utils/localization/ThTranslation';
+import { GoogleAnalytics } from '../../../common/utils/analytics/GoogleAnalytics';
+import { IAnalytics } from '../../../common/utils/analytics/IAnalytics';
+import { AppContext } from '../../../common/utils/AppContext';
+import { ModuleLoaderService } from '../../../common/utils/module-loader/ModuleLoaderService';
 
-import {SharedDirectivesModule} from '../../../common/utils/directives/modules/SharedDirectivesModule';
-import {SharedPipesModule} from '../../../common/utils/pipes/modules/SharedPipesModule';
-import {ExternalPagesModule} from '../pages/ExternalPagesModule';
+import { SharedDirectivesModule } from '../../../common/utils/directives/modules/SharedDirectivesModule';
+import { SharedPipesModule } from '../../../common/utils/pipes/modules/SharedPipesModule';
+import { ExternalPagesModule } from '../pages/ExternalPagesModule';
 
 @NgModule({
-    imports: [BrowserModule, HttpModule, RouterModule, ExternalRouting,
+	imports: [BrowserModule, HttpModule, RouterModule, ExternalRouting,
 		SharedDirectivesModule, SharedPipesModule, ExternalPagesModule],
-    declarations: [MainLayoutExternalComponent],
-    bootstrap: [MainLayoutExternalComponent],
+	declarations: [MainLayoutExternalComponent],
+	bootstrap: [MainLayoutExternalComponent],
 	providers: [
 		{ provide: IThCookie, useClass: ThCookie },
 		{ provide: IBrowserLocation, useClass: BrowserLocation },
@@ -51,4 +51,7 @@ import {ExternalPagesModule} from '../pages/ExternalPagesModule';
 })
 export class ExternalModule { }
 
+if ((<any>window).enableAngularProdMode === true) {
+	enableProdMode();
+}
 platformBrowserDynamic().bootstrapModule(ExternalModule);
