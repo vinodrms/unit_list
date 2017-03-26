28eea5c03205f161071bdd828e71eb59ff5ba528
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-03-26 12:49:57 +0300

    #246 Fixed ddr issues

--- assets/client/src/pages/internal/containers/common/inventory/price-products/pages/price-product-edit/sections/prices/utils/DynamicPriceVMContainer.ts
+++ assets/client/src/pages/internal/containers/common/inventory/price-products/pages/price-product-edit/sections/prices/utils/DynamicPriceVMContainer.ts
@@ -10,49 +10,57 @@ import { ThUtils } from "../../../../../../../../../../../common/utils/ThUtils";
 
 export class DynamicPriceVMContainer {
     private _thUtils: ThUtils;
-    
+
     private _selectedDynamicPriceIndex: number;
     private _dynamicPriceVMList: DynamicPriceVM[];
-    
-    constructor(private _priceType: PriceProductPriceType) {
-        this._selectedDynamicPriceIndex = 0;
 
+    constructor(private _priceType: PriceProductPriceType) {
         this._thUtils = new ThUtils();
+
+        let defaultDynamicPriceVM = new DynamicPriceVM(_priceType);
+        defaultDynamicPriceVM.name = "Default Pricing";
+        defaultDynamicPriceVM.description = "";
+        defaultDynamicPriceVM.priceVMList = [];
+        defaultDynamicPriceVM.selected = true;
+        this._selectedDynamicPriceIndex = 0;
+        this._dynamicPriceVMList = [defaultDynamicPriceVM];
     }
 
     public get priceType(): PriceProductPriceType {
         return this._priceType;
     }
-    
+
     public initializeFrom(price: PriceProductPriceDO) {
         let newDynamicPriceVMList: DynamicPriceVM[] = [];
         _.forEach(price.dynamicPriceList, (dynamicPrice: DynamicPriceDO) => {
             let dynamicPriceVM = new DynamicPriceVM(this._priceType);
             dynamicPriceVM.initializeFrom(dynamicPrice);
-            
+
             newDynamicPriceVMList.push(dynamicPriceVM);
         });
         this._dynamicPriceVMList = newDynamicPriceVMList;
 
-        if(this._dynamicPriceVMList.length > 0) {
+        if (this._dynamicPriceVMList.length > 0) {
             this._selectedDynamicPriceIndex = 0;
             this.selectDynamicPrice(this._selectedDynamicPriceIndex);
         }
     }
 
     public get selectedDynamicPriceIndex(): number {
-		return this._selectedDynamicPriceIndex;
-	}
+        return this._selectedDynamicPriceIndex;
+    }
 
     public selectDynamicPrice(index: number) {
-		this.dynamicPriceVMList[this._selectedDynamicPriceIndex].selected = false;
-		this.dynamicPriceVMList[index].selected = true;
-		this._selectedDynamicPriceIndex = index;
+        this.dynamicPriceVMList.forEach(dynamicPrice => {
+            dynamicPrice.selected = false;
+        });
+        this.dynamicPriceVMList[index].selected = true;
+        this._selectedDynamicPriceIndex = index;
     }
 
     public get selectedDynamicPriceVM(): DynamicPriceVM {
-		return this._dynamicPriceVMList[this._selectedDynamicPriceIndex];
-	}
+        return this._dynamicPriceVMList[this._selectedDynamicPriceIndex];
+    }
 
     public get dynamicPriceVMList(): DynamicPriceVM[] {
         return this._dynamicPriceVMList;
@@ -65,7 +73,7 @@ export class DynamicPriceVMContainer {
     public isValid(): boolean {
         let isValid = true;
         _.forEach(this._dynamicPriceVMList, (dynamicPriceVM: DynamicPriceVM) => {
-            if(!dynamicPriceVM.isValid()) {
+            if (!dynamicPriceVM.isValid()) {
                 isValid = false;
             }
         });
@@ -78,12 +86,12 @@ export class DynamicPriceVMContainer {
         priceProductVM.priceProduct.price.type = this._priceType;
 
         _.forEach(this._dynamicPriceVMList, (dynamicPriceVM: DynamicPriceVM) => {
-            dynamicPriceVM.updatePricesOn(priceProductVM);    
+            dynamicPriceVM.updatePricesOn(priceProductVM);
         });
     }
 
     private removeDeletedDynamicPricesOnPriceProduct(priceProductVM: PriceProductVM) {
-        let dynamicPriceIdList = 
+        let dynamicPriceIdList =
             _.chain(this._dynamicPriceVMList)
                 .map((dynamicPriceVM: DynamicPriceVM) => {
                     return dynamicPriceVM.dynamicPriceDO.id
@@ -92,14 +100,14 @@ export class DynamicPriceVMContainer {
                     return !this._thUtils.isUndefinedOrNull(dynamicPriceId);
                 })
                 .value();
-        
-        priceProductVM.priceProduct.price.dynamicPriceList = 
+
+        priceProductVM.priceProduct.price.dynamicPriceList =
             _.filter(priceProductVM.priceProduct.price.dynamicPriceList, (dynamicPriceDO: DynamicPriceDO) => {
                 return _.contains(dynamicPriceIdList, dynamicPriceDO.id);
             });
     }
 
-    public updateFromRoomCategoryStatsList(roomCategoryStatsList: RoomCategoryStatsDO[]) { 
+    public updateFromRoomCategoryStatsList(roomCategoryStatsList: RoomCategoryStatsDO[]) {
         _.forEach(this._dynamicPriceVMList, (dynamicPriceVM: DynamicPriceVM) => {
             dynamicPriceVM.updateFromRoomCategoryStatsList(roomCategoryStatsList);
         });
