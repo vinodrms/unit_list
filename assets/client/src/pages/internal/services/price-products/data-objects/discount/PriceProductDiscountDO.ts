43e04d4839f31acdcac874ac78130d9d3cf11ef4
Dragos Pricope <dragos.pricope@3angle.tech>
2017-03-30 18:52:39 +0300

    #259 Refactored PriceProductDO on the client

--- assets/client/src/pages/internal/services/price-products/data-objects/discount/PriceProductDiscountDO.ts
+++ assets/client/src/pages/internal/services/price-products/data-objects/discount/PriceProductDiscountDO.ts
@@ -1,12 +1,14 @@
 import { BaseDO } from '../../../../../../common/base/BaseDO';
 import { PriceProductConstraintWrapperDO } from "../constraint/PriceProductConstraintWrapperDO";
+import { ThDateIntervalDO } from "../../../common/data-objects/th-dates/ThDateIntervalDO";
 
 export class PriceProductDiscountDO extends BaseDO {
     name: string;
     value: number;
     constraints: PriceProductConstraintWrapperDO;
     customerIdList: string[];
-
+    intervals: ThDateIntervalDO[];
+    
     protected getPrimitivePropertyKeys(): string[] {
         return ["name", "value", "customerIdList"];
     }
@@ -16,6 +18,13 @@ export class PriceProductDiscountDO extends BaseDO {
 
         this.constraints = new PriceProductConstraintWrapperDO();
         this.constraints.buildFromObject(this.getObjectPropertyEnsureUndefined(object, "constraints"));
+
+        this.intervals = [];
+		this.forEachElementOf(this.getObjectPropertyEnsureUndefined(object, "intervals"), (intervalObject: Object) => {
+			var dateIntervalDO = new ThDateIntervalDO();
+			dateIntervalDO.buildFromObject(intervalObject);
+			this.intervals.push(dateIntervalDO);
+		});
     }
 
     public isValid(): boolean {
