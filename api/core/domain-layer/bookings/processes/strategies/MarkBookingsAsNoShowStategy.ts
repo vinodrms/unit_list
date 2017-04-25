37fa4711ef950ee1eb16ec22a41c59e8e5b4030e
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-04-26 02:26:38 +0300

    #163 Do not recompute booking's penalty price if its invoice is closed

--- api/core/domain-layer/bookings/processes/strategies/MarkBookingsAsNoShowStategy.ts
+++ api/core/domain-layer/bookings/processes/strategies/MarkBookingsAsNoShowStategy.ts
@@ -1,14 +1,18 @@
-import {IBookingProcessStrategy, BookingStrategyMatchParams} from './IBookingProcessStrategy';
-import {BookingSearchCriteriaRepoDO} from '../../../../data-layer/bookings/repositories/IBookingRepository';
-import {BookingDO, BookingConfirmationStatus} from '../../../../data-layer/bookings/data-objects/BookingDO';
-import {DocumentActionDO} from '../../../../data-layer/common/data-objects/document-history/DocumentActionDO';
-import {BookingPriceType} from '../../../../data-layer/bookings/data-objects/price/BookingPriceDO';
-import {BookingUtils} from '../../utils/BookingUtils';
+import { AppContext } from "../../../../utils/AppContext";
+import { SessionContext } from "../../../../utils/SessionContext";
+import { ThError } from "../../../../utils/th-responses/ThError";
+import { IBookingProcessStrategy, BookingStrategyMatchParams } from './IBookingProcessStrategy';
+import { BookingSearchCriteriaRepoDO } from '../../../../data-layer/bookings/repositories/IBookingRepository';
+import { BookingDO, BookingConfirmationStatus } from '../../../../data-layer/bookings/data-objects/BookingDO';
+import { DocumentActionDO } from '../../../../data-layer/common/data-objects/document-history/DocumentActionDO';
+import { BookingPriceType } from '../../../../data-layer/bookings/data-objects/price/BookingPriceDO';
+import { BookingUtils } from '../../utils/BookingUtils';
+import { BookingWithDependenciesLoader } from "../../../hotel-operations/booking/utils/BookingWithDependenciesLoader";
 
 export class MarkBookingsAsNoShowStategy implements IBookingProcessStrategy {
     private _bookingUtils: BookingUtils;
 
-    constructor() {
+    constructor(private _appContext: AppContext, private _sessionContext: SessionContext) {
         this._bookingUtils = new BookingUtils();
     }
 
@@ -23,12 +27,34 @@ export class MarkBookingsAsNoShowStategy implements IBookingProcessStrategy {
         };
     }
 
-    public updateMatchedBooking(bookingDO: BookingDO, params: BookingStrategyMatchParams) {
-        if (this.hasPenalty(bookingDO, params)) {
-            this.updateBookingAsNoShowWithPenalty(bookingDO);
+    public updateMatchedBooking(bookingDO: BookingDO, params: BookingStrategyMatchParams): Promise<BookingDO> {
+        return new Promise<BookingDO>((resolve: { (result: BookingDO): void }, reject: { (err: ThError): void }) => {
+            this.updateMatchedBookingCore(resolve, reject, bookingDO, params);
+        });
+    }
+    private updateMatchedBookingCore(resolve: { (result: BookingDO): void }, reject: { (err: ThError): void },
+        bookingDO: BookingDO, params: BookingStrategyMatchParams) {
+        if (!this.hasPenalty(bookingDO, params)) {
+            this.updateBookingAsNoShow(bookingDO);
+            resolve(bookingDO);
             return;
         }
-        this.updateBookingAsNoShow(bookingDO);
+
+        let loader = new BookingWithDependenciesLoader(this._appContext, this._sessionContext);
+        loader.load(bookingDO.groupBookingId, bookingDO.bookingId)
+            .then(bookingWithDeps => {
+                // only update the penalty if the booking's invoice is not paid
+                if (!bookingWithDeps.hasClosedInvoice()) {
+                    this.updateBookingAsNoShowWithPenalty(bookingDO);
+                }
+                else {
+                    // if the booking already has a paid invoice just log an according message
+                    this.updateBookingAsNoShowWithWarning(bookingDO);
+                }
+                resolve(bookingDO);
+            }).catch(e => {
+                reject(e);
+            });
     }
 
     private hasPenalty(bookingDO: BookingDO, params: BookingStrategyMatchParams): boolean {
@@ -53,6 +79,13 @@ export class MarkBookingsAsNoShowStategy implements IBookingProcessStrategy {
         bookingDO.bookingHistory.logDocumentAction(DocumentActionDO.buildDocumentActionDO({
             actionParameterMap: {},
             actionString: "Booking was marked as No Show by the System. The booking has a penalty on the attached invoice."
+        }));
+    }
+    private updateBookingAsNoShowWithWarning(bookingDO: BookingDO) {
+        bookingDO.confirmationStatus = BookingConfirmationStatus.NoShow;
+        bookingDO.bookingHistory.logDocumentAction(DocumentActionDO.buildDocumentActionDO({
+            actionParameterMap: {},
+            actionString: "Booking was marked as No Show by the System. The penalty could not be generated because the invoice is already closed."
         }));
     }
 }
