3cb3d13df9bbb7dddddf6ac9dfedbc5e6e3a0286
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-04-26 01:40:00 +0300

    #163 Refactored the GenerateBookingInvoice

--- api/core/domain-layer/invoices/generate-booking-invoice/GenerateBookingInvoice.ts
+++ api/core/domain-layer/invoices/generate-booking-invoice/GenerateBookingInvoice.ts
@@ -7,7 +7,6 @@ import { SessionContext } from '../../../utils/SessionContext';
 import { ValidationResultParser } from '../../common/ValidationResultParser';
 import { GenerateBookingInvoiceDO, GenerateBookingInvoiceAopMeta } from './GenerateBookingInvoiceDO';
 import { InvoiceGroupDO } from '../../../data-layer/invoices/data-objects/InvoiceGroupDO';
-import { BookingIdValidator } from '../validators/BookingIdValidator';
 import { BookingDO } from '../../../data-layer/bookings/data-objects/BookingDO';
 import { InvoiceDO, InvoicePaymentStatus } from '../../../data-layer/invoices/data-objects/InvoiceDO';
 import { InvoiceItemDO, InvoiceItemType } from '../../../data-layer/invoices/data-objects/items/InvoiceItemDO';
@@ -47,26 +46,27 @@ export class GenerateBookingInvoice {
     }
 
     private generateCore(resolve: { (result: InvoiceGroupDO): void }, reject: { (err: ThError): void }) {
-        var bookingIdValidator = new BookingIdValidator(this._appContext, this._sessionContext);
-        bookingIdValidator.validateBookingId(this._generateBookingInvoiceDO.groupBookingId, this._generateBookingInvoiceDO.bookingId).then((booking: BookingDO) => {
-            this._loadedBooking = booking;
+        var bookignRepo = this._appContext.getRepositoryFactory().getBookingRepository();
+        bookignRepo.getBookingById({ hotelId: this._sessionContext.sessionDO.hotel.id }, this._generateBookingInvoiceDO.groupBookingId, this._generateBookingInvoiceDO.bookingId)
+            .then((booking: BookingDO) => {
+                this._loadedBooking = booking;
 
-            var customerIdValidator = new CustomerIdValidator(this._appContext, this._sessionContext);
-            return customerIdValidator.validateCustomerIdList([this._loadedBooking.defaultBillingDetails.customerId]);
-        }).then((loadedCustomersContainer: CustomersContainer) => {
-            this._loadedDefaultBillingCustomer = loadedCustomersContainer.customerList[0];
+                var customerIdValidator = new CustomerIdValidator(this._appContext, this._sessionContext);
+                return customerIdValidator.validateCustomerIdList([this._loadedBooking.defaultBillingDetails.customerId]);
+            }).then((loadedCustomersContainer: CustomersContainer) => {
+                this._loadedDefaultBillingCustomer = loadedCustomersContainer.customerList[0];
 
-            return this.getDefaultInvoiceDO();
-        }).then((defaultInvoice: InvoiceDO) => {
-            var generateBookingInvoiceActionFactory = new GenerateBookingInvoiceActionFactory(this._appContext, this._sessionContext);
-            return generateBookingInvoiceActionFactory.getActionStrategy(this._loadedBooking.groupBookingId, defaultInvoice);
-        }).then((actionStrategy: IGenerateBookingInvoiceActionStrategy) => {
-            actionStrategy.generateBookingInvoice(resolve, reject);
-        }).catch((error: any) => {
-            var thError = new ThError(ThStatusCode.GenerateBookingInvoiceError, error);
-            ThLogger.getInstance().logError(ThLogLevel.Error, "error generating invoice related to booking", this._generateBookingInvoiceDO, thError);
-            reject(thError);
-        });
+                return this.getDefaultInvoiceDO();
+            }).then((defaultInvoice: InvoiceDO) => {
+                var generateBookingInvoiceActionFactory = new GenerateBookingInvoiceActionFactory(this._appContext, this._sessionContext);
+                return generateBookingInvoiceActionFactory.getActionStrategy(this._loadedBooking.groupBookingId, defaultInvoice);
+            }).then((actionStrategy: IGenerateBookingInvoiceActionStrategy) => {
+                actionStrategy.generateBookingInvoice(resolve, reject);
+            }).catch((error: any) => {
+                var thError = new ThError(ThStatusCode.GenerateBookingInvoiceError, error);
+                ThLogger.getInstance().logError(ThLogLevel.Error, "error generating invoice related to booking", this._generateBookingInvoiceDO, thError);
+                reject(thError);
+            });
     }
 
     private getDefaultInvoiceDO(): Promise<InvoiceDO> {
@@ -85,52 +85,50 @@ export class GenerateBookingInvoice {
         invoice.itemList.push(bookingInvoiceItem);
         invoice.paymentStatus = InvoicePaymentStatus.Unpaid;
 
-        this._appContext.getRepositoryFactory().getHotelRepository().getHotelById(this.hotelId)
 
-        this._appContext.getRepositoryFactory().getBookingRepository().getBookingById({ hotelId: this.hotelId }, this._generateBookingInvoiceDO.groupBookingId,
-            this._generateBookingInvoiceDO.bookingId).then((booking: BookingDO) => {
-                invoice.payerList = [];
-                var defaultInvoicePayer =
-                    InvoicePayerDO.buildFromCustomerDOAndPaymentMethod(this._loadedDefaultBillingCustomer, this._loadedBooking.defaultBillingDetails.paymentMethod);
+        invoice.payerList = [];
+        var defaultInvoicePayer =
+            InvoicePayerDO.buildFromCustomerDOAndPaymentMethod(this._loadedDefaultBillingCustomer, this._loadedBooking.defaultBillingDetails.paymentMethod);
 
-                defaultInvoicePayer.priceToPay = this._thUtils.roundNumberToTwoDecimals(booking.price.totalBookingPrice);
+        defaultInvoicePayer.priceToPay = this._thUtils.roundNumberToTwoDecimals(this._loadedBooking.price.totalBookingPrice);
 
-                if (defaultInvoicePayer.paymentMethod.type === InvoicePaymentMethodType.PayInvoiceByAgreement) {
-                    var corporateDetails = new BaseCorporateDetailsDO();
-                    corporateDetails.buildFromObject(this._loadedDefaultBillingCustomer.customerDetails);
+        if (defaultInvoicePayer.paymentMethod.type === InvoicePaymentMethodType.PayInvoiceByAgreement) {
+            var corporateDetails = new BaseCorporateDetailsDO();
+            corporateDetails.buildFromObject(this._loadedDefaultBillingCustomer.customerDetails);
 
-                    defaultInvoicePayer.priceToPay =
-                        this._thUtils.roundNumberToTwoDecimals(defaultInvoicePayer.priceToPay + corporateDetails.invoiceFee);
-                }
+            defaultInvoicePayer.priceToPay =
+                this._thUtils.roundNumberToTwoDecimals(defaultInvoicePayer.priceToPay + corporateDetails.invoiceFee);
+        }
 
-                invoice.payerList.push(defaultInvoicePayer);
+        invoice.payerList.push(defaultInvoicePayer);
 
-                _.forEach(this._generateBookingInvoiceDO.initialAddOnProducts, (generateAopMeta: GenerateBookingInvoiceAopMeta) => {
-                    var aopInvoiceItem = new InvoiceItemDO();
-                    aopInvoiceItem.buildFromAddOnProductDO(generateAopMeta.addOnProductDO, generateAopMeta.noOfItems, true, generateAopMeta.addOnProductDO.getVatId());
-                    invoice.itemList.push(aopInvoiceItem);
-                });
+        _.forEach(this._generateBookingInvoiceDO.initialAddOnProducts, (generateAopMeta: GenerateBookingInvoiceAopMeta) => {
+            var aopInvoiceItem = new InvoiceItemDO();
+            aopInvoiceItem.buildFromAddOnProductDO(generateAopMeta.addOnProductDO, generateAopMeta.noOfItems, true, generateAopMeta.addOnProductDO.getVatId());
+            invoice.itemList.push(aopInvoiceItem);
+        });
 
-                invoice.notesFromBooking = booking.invoiceNotes;
-                
-                return this._appContext.getRepositoryFactory().getHotelRepository().getHotelById(this.hotelId);
-            }).then((hotel: HotelDO) => {
-                let transactionFee: TransactionFeeDO = null;
-                
-                if(invoice.payerList[0].paymentMethod.type != InvoicePaymentMethodType.PayInvoiceByAgreement) {
-                    transactionFee = _.find(hotel.paymentMethodList, (paymentMethodInstance: PaymentMethodInstanceDO) => {
-                        return paymentMethodInstance.paymentMethodId === invoice.payerList[0].paymentMethod.value;
-                    }).transactionFee;
-                }
-                else {
-                    transactionFee = TransactionFeeDO.getDefaultTransactionFee();
-                }
-                
-                invoice.payerList[0].transactionFeeSnapshot = transactionFee;
-                invoice.payerList[0].priceToPayPlusTransactionFee = transactionFee.getAmountWihtTransactionFeeIncluded(invoice.payerList[0].priceToPay);
+        invoice.notesFromBooking = this._loadedBooking.invoiceNotes;
 
-                resolve(invoice);
-            });
+        this._appContext.getRepositoryFactory().getHotelRepository().getHotelById(this.hotelId).then((hotel: HotelDO) => {
+            let transactionFee: TransactionFeeDO = null;
+
+            if (invoice.payerList[0].paymentMethod.type != InvoicePaymentMethodType.PayInvoiceByAgreement) {
+                transactionFee = _.find(hotel.paymentMethodList, (paymentMethodInstance: PaymentMethodInstanceDO) => {
+                    return paymentMethodInstance.paymentMethodId === invoice.payerList[0].paymentMethod.value;
+                }).transactionFee;
+            }
+            else {
+                transactionFee = TransactionFeeDO.getDefaultTransactionFee();
+            }
+
+            invoice.payerList[0].transactionFeeSnapshot = transactionFee;
+            invoice.payerList[0].priceToPayPlusTransactionFee = transactionFee.getAmountWihtTransactionFeeIncluded(invoice.payerList[0].priceToPay);
+
+            resolve(invoice);
+        }).catch(e => {
+            reject(e);
+        })
     }
 
     private get hotelId(): string {
