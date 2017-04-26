094a3d354f49a268a1de2c53ef5c73ae7f962a19
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-04-26 16:42:38 +0300

    #163 Added support for generating the invoice from the booking operations page

--- assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/operations-modal/components/components/booking-operations/components/booking-links/BookingLinksComponent.ts
+++ assets/client/src/pages/internal/containers/home/pages/home-pages/hotel-operations/operations-modal/components/components/booking-operations/components/booking-links/BookingLinksComponent.ts
@@ -1,7 +1,9 @@
-import {Component, Input} from '@angular/core';
-import {AppContext} from '../../../../../../../../../../../../../common/utils/AppContext';
-import {HotelOperationsPageControllerService} from '../../../../services/HotelOperationsPageControllerService';
-import {BookingOperationsPageData} from '../../services/utils/BookingOperationsPageData';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+import { AppContext, ThError } from '../../../../../../../../../../../../../common/utils/AppContext';
+import { HotelOperationsPageControllerService } from '../../../../services/HotelOperationsPageControllerService';
+import { BookingOperationsPageData } from '../../services/utils/BookingOperationsPageData';
+import { HotelOperationsBookingService } from "../../../../../../../../../../../services/hotel-operations/booking/HotelOperationsBookingService";
+import { InvoiceGroupDO } from "../../../../../../../../../../../services/invoices/data-objects/InvoiceGroupDO";
 
 @Component({
     selector: 'booking-links',
@@ -10,8 +12,16 @@ import {BookingOperationsPageData} from '../../services/utils/BookingOperationsP
 export class BookingLinksComponent {
     @Input() bookingOperationsPageData: BookingOperationsPageData;
 
+    @Output() onInvoiceGenerated = new EventEmitter<InvoiceGroupDO>();
+    public triggerOnInvoiceGenerated(invoiceGroup: InvoiceGroupDO) {
+        this.onInvoiceGenerated.next(invoiceGroup);
+    }
+
+    private isSaving: boolean = false;
+
     constructor(private _appContext: AppContext,
-        private _operationsPageControllerService: HotelOperationsPageControllerService) { }
+        private _operationsPageControllerService: HotelOperationsPageControllerService,
+        private _hotelOperationsBookingService: HotelOperationsBookingService) { }
 
     public get hasInvoice(): boolean {
         return this.bookingOperationsPageData.hasInvoice;
@@ -32,5 +42,27 @@ export class BookingLinksComponent {
     public viewRoom() {
         if (!this.hasRoom) { return; }
         this._operationsPageControllerService.goToRoom(this.bookingOperationsPageData.bookingDO.roomId);
+    }
+
+    private generateInvoice() {
+        if (this.isSaving) { return; }
+        var title = this._appContext.thTranslation.translate("Generate Invoice");
+        var content = this._appContext.thTranslation.translate("Are you sure you want to generate the associated invoice for this booking?");
+        this._appContext.modalService.confirm(title, content, { positive: this._appContext.thTranslation.translate("Yes"), negative: this._appContext.thTranslation.translate("No") },
+            () => {
+                this.generateInvoiceCore();
+            }, () => { });
+    }
+    private generateInvoiceCore() {
+        this.isSaving = true;
+        this._hotelOperationsBookingService.generateInvoice(this.bookingOperationsPageData.bookingDO)
+            .subscribe((updatedBooking: InvoiceGroupDO) => {
+                this._appContext.analytics.logEvent("booking", "generate-invoice", "Generated an invoice for a booking");
+                this.isSaving = false;
+                this.triggerOnInvoiceGenerated(updatedBooking);
+            }, (error: ThError) => {
+                this.isSaving = false;
+                this._appContext.toaster.error(error.message);
+            });
     }
 }
