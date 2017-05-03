9ef2cfef08f44794d344de66cfc176b3e68105c8
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-05-03 12:41:09 +0300

    #292 Fixed the migration script

--- api/core/bootstrap/db-patches/mongo/patch-applier/patches/list/P24_MigrateBookingGroupsToBookings.ts
+++ api/core/bootstrap/db-patches/mongo/patch-applier/patches/list/P24_MigrateBookingGroupsToBookings.ts
@@ -7,6 +7,8 @@ import { InvoiceGroupSearchResultRepoDO } from "../../../../../../data-layer/inv
 import { InvoiceGroupDO } from "../../../../../../data-layer/invoices/data-objects/InvoiceGroupDO";
 import { InvoiceDO } from "../../../../../../data-layer/invoices/data-objects/InvoiceDO";
 import { InvoiceItemDO, InvoiceItemType } from "../../../../../../data-layer/invoices/data-objects/items/InvoiceItemDO";
+import { MongoBookingRepository } from "../../../../../../data-layer/bookings/repositories/mongo/MongoBookingRepository";
+import { MongoInvoiceGroupsRepository } from "../../../../../../data-layer/invoices/repositories/mongo/MongoInvoiceGroupsRepository";
 
 import _ = require('underscore');
 
@@ -39,6 +41,20 @@ export class P24_MigrateBookingGroupsToBookings extends APaginatedTransactionalM
         });
     }
     private updateCore(resolve: { (result: any): void }, reject: { (err: ThError): void }, bookingGroup, bookingItem) {
+        let migrator = new BookingMigrator(this._bookingRepository, this._invoiceGroupsRepository);
+        migrator.migrate(resolve, reject, bookingGroup, bookingItem);
+    }
+    protected updateDocumentInMemory(bookingGroup) { }
+}
+
+class BookingMigrator {
+    private _legacyBookingId: string;
+    private _newBookingId: string;
+
+    constructor(private _bookingRepository: MongoBookingRepository, private _invoiceGroupsRepository: MongoInvoiceGroupsRepository) {
+    }
+
+    public migrate(resolve: { (result: any): void }, reject: { (err: ThError): void }, bookingGroup, bookingItem) {
         if (bookingItem.migrated === true) {
             resolve(bookingItem);
             return;
@@ -54,21 +70,20 @@ export class P24_MigrateBookingGroupsToBookings extends APaginatedTransactionalM
         booking.inputChannel = bookingGroup.inputChannel;
         booking.noOfRooms = bookingGroup.noOfRooms;
 
-        let legacyBookingId = bookingItem.bookingId;
-        var bookingId;
+        this._legacyBookingId = bookingItem.bookingId;
 
         this._bookingRepository.addBookings({ hotelId: booking.hotelId }, [booking])
             .then((convertedBookingList: BookingDO[]) => {
                 let convertedBooking = convertedBookingList[0];
-                bookingId = convertedBooking.id;
+                this._newBookingId = convertedBooking.id;
 
                 return this._invoiceGroupsRepository.getInvoiceGroupList({ hotelId: booking.hotelId }, {
-                    bookingId: legacyBookingId
+                    bookingId: this._legacyBookingId
                 });
             }).then((searchResult: InvoiceGroupSearchResultRepoDO) => {
                 let promiseList: Promise<InvoiceGroupDO>[] = [];
                 searchResult.invoiceGroupList.forEach(invoiceGroup => {
-                    promiseList.push(this.updateBookingIdOnInvoiceGroup(invoiceGroup, legacyBookingId, bookingId));
+                    promiseList.push(this.updateBookingIdOnInvoiceGroup(invoiceGroup, this._legacyBookingId, this._newBookingId));
                 });
                 return Promise.all(promiseList);
             }).then(updatedInvoiceGroupList => {
@@ -79,22 +94,28 @@ export class P24_MigrateBookingGroupsToBookings extends APaginatedTransactionalM
             });
     }
 
-    private updateBookingIdOnInvoiceGroup(invoiceGroup: InvoiceGroupDO, legacyBookingId: string, bookingId: string): Promise<InvoiceGroupDO> {
-        invoiceGroup.invoiceList.forEach((invoice: InvoiceDO) => {
-            if (invoice.bookingId === legacyBookingId) {
-                invoice.bookingId = bookingId;
-            }
-            invoice.itemList.forEach((item: InvoiceItemDO) => {
-                if (item.type === InvoiceItemType.Booking && item.id === legacyBookingId) {
-                    item.id = bookingId;
+    private updateBookingIdOnInvoiceGroup(invoiceGroup: InvoiceGroupDO, legacyBookingId: string, newBookingId: string): Promise<InvoiceGroupDO> {
+        return new Promise<InvoiceGroupDO>((resolve: { (result: InvoiceGroupDO): void }, reject: { (err: ThError): void }) => {
+            invoiceGroup.invoiceList = _.map(invoiceGroup.invoiceList, (invoice: InvoiceDO) => {
+                if (invoice.bookingId === legacyBookingId) {
+                    invoice.bookingId = newBookingId;
                 }
+                invoice.itemList = _.map(invoice.itemList, (item: InvoiceItemDO) => {
+                    if (item.type === InvoiceItemType.Booking && item.id === legacyBookingId) {
+                        item.id = newBookingId;
+                    }
+                    return item;
+                });
+                return invoice;
+            });
+            this._invoiceGroupsRepository.updateInvoiceGroup({ hotelId: invoiceGroup.hotelId }, {
+                id: invoiceGroup.id,
+                versionId: invoiceGroup.versionId
+            }, invoiceGroup).then((updatedGroup: InvoiceGroupDO) => {
+                resolve(updatedGroup);
+            }).catch(e => {
+                reject(e);
             });
         });
-        return this._invoiceGroupsRepository.updateInvoiceGroup({ hotelId: invoiceGroup.hotelId }, {
-            id: invoiceGroup.id,
-            versionId: invoiceGroup.versionId
-        }, invoiceGroup);
     }
-
-    protected updateDocumentInMemory(bookingGroup) { }
 }
