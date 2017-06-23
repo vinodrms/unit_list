f6c3e3a08647474388f95bf0188475011567bc68
Dragos Pricope <dragos.pricope@3angle.tech>
2017-06-23 10:44:02 +0300

    #178 Added weekend and week days guest nights counters

--- api/core/domain-layer/reports/monthly-stats/sections/MonthlyStatsReportNoOfGuestNightsSectionGenerator.ts
+++ api/core/domain-layer/reports/monthly-stats/sections/MonthlyStatsReportNoOfGuestNightsSectionGenerator.ts
@@ -7,11 +7,15 @@ import { ThError } from "../../../../utils/th-responses/ThError";
 import { BookingDOConstraints } from "../../../../data-layer/bookings/data-objects/BookingDOConstraints";
 import { BookingSearchResultRepoDO } from "../../../../data-layer/bookings/repositories/IBookingRepository";
 import { BookingDO } from "../../../../data-layer/bookings/data-objects/BookingDO";
+import { IndexedBookingInterval } from "../../../../data-layer/price-products/utils/IndexedBookingInterval";
+import { ISOWeekDay } from "../../../../utils/th-dates/data-objects/ISOWeekDay";
 
 import _ = require('underscore');
 
-export class MonthlyStatsReportNoOfNightsSectionGenerator extends AReportSectionGeneratorStrategy {
+export class MonthlyStatsReportNoOfGuestNightsSectionGenerator extends AReportSectionGeneratorStrategy {
     private _totalGuestNights: number = 0;
+    private _weekdaysGuestNights: number = 0;
+    private _weekendGuestNights: number = 0;
 
     constructor(appContext: AppContext, sessionContext: SessionContext, globalSummary: Object,
         private _bookingList: BookingDO[]) {
@@ -20,7 +24,7 @@ export class MonthlyStatsReportNoOfNightsSectionGenerator extends AReportSection
 
     protected getMeta(): ReportSectionMeta {
         return {
-            title: "Total guest nights"
+            title: "Guest nights"
         }
     }
 
@@ -31,6 +35,8 @@ export class MonthlyStatsReportNoOfNightsSectionGenerator extends AReportSection
     protected getLocalSummary(): Object {
         return {
             "Total guest nigths": this._totalGuestNights,
+            "Total guest nights weekdays": this._weekdaysGuestNights,
+            "Total guest nights weekend": this._weekendGuestNights,
 
         }
     }
@@ -47,7 +53,24 @@ export class MonthlyStatsReportNoOfNightsSectionGenerator extends AReportSection
     protected getDataCore(resolve: (result: any[][]) => void, reject: (err: ThError) => void) {
         this._totalGuestNights = _.reduce(this._bookingList, (sum, booking: BookingDO) => {
             return sum + booking.getNumberOfGuestNights();
-        }, 0);
+        }, 0);        
+
+        _.forEach(this._bookingList, (booking: BookingDO) => {
+            let indexedInterval = new IndexedBookingInterval(booking.interval);
+            
+            let weekendDays = [ISOWeekDay.Saturday, ISOWeekDay.Sunday];
+            let noOfWeekendNights = 0;
+            _.forEach(indexedInterval.bookingISOWeekDayList, (day: ISOWeekDay) => {
+                if(_.contains(weekendDays, day)) {
+                    noOfWeekendNights++;
+                }
+            });
+
+            let noOfWeekNights = booking.getNumberOfNights() - noOfWeekendNights;
+
+            this._weekdaysGuestNights += booking.configCapacity.getTotalNumberOfGuests() * noOfWeekNights;
+            this._weekendGuestNights += booking.configCapacity.getTotalNumberOfGuests() * noOfWeekendNights;
+        });
 
         resolve([]);
     }
