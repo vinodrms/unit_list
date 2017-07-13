8d79bbb3b92f5ecb8ea41e3b45d5d9c511545dd1
Dragos Pricope <dragos.pricope@3angle.tech>
2017-07-13 16:33:47 +0300

    [HotFix] Run ansible with verbose option enabled

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -26,7 +26,7 @@ mkdir $environment
 cd $environment
 git clone https://gitlabrunner:Gts%2527_217AZC%23%24%252_982@gitlab.3angletech.com/UnitPalDK/UnitPal.git --branch $branch
 cd UnitPal/ansible
-ansible-playbook -i inventories/$inventory/hosts --private-key=~/.ssh/unitpal_rsa -u unitpal unitpal_update.yml
+ansible-playbook -vvv -i inventories/$inventory/hosts --private-key=~/.ssh/unitpal_rsa -u unitpal unitpal_update.yml
 cd ../../../
 rm -rf $environment
 echo "Finished install $environment on Azure"
