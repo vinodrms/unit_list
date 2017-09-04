6fbf44434fecabe06bbdfe530b2b464a8910d543
Dragos Pricope <dragos.pricope@3angle.tech>
2017-09-04 10:00:29 +0300

    #5 Fixed broken tests

--- api/test/root/unit-tests/user-account/UserAccountTest.ts
+++ api/test/root/unit-tests/user-account/UserAccountTest.ts
@@ -2,46 +2,40 @@ require("sails-test-helper");
 import should = require('should');
 import supertest = require('supertest');
 
-import {TestContext} from '../../../helpers/TestContext';
-import {ThError} from '../../../../core/utils/th-responses/ThError';
-import {ThStatusCode, ThResponse} from '../../../../core/utils/th-responses/ThResponse';
-import {UserDO} from '../../../../core/data-layer/hotel/data-objects/user/UserDO';
-import {HotelSignUp} from '../../../../core/domain-layer/hotel-account/sign-up/HotelSignUp';
-import {HotelSignUpDO} from '../../../../core/domain-layer/hotel-account/sign-up/HotelSignUpDO';
-import {ActionTokenDO} from '../../../../core/data-layer/hotel/data-objects/user/ActionTokenDO';
-import {UserAccountActivation} from '../../../../core/domain-layer/hotel-account/account-activation/UserAccountActivation';
-import {UserAccountActivationDO} from '../../../../core/domain-layer/hotel-account/account-activation/UserAccountActivationDO';
-import {UserAccountRequestResetPassword} from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountRequestResetPassword';
-import {UserAccountRequestResetPasswordDO} from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountRequestResetPasswordDO';
-import {UserAccountResetPassword} from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountResetPassword';
-import {UserAccountResetPasswordDO} from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountResetPasswordDO';
+import { TestContext } from '../../../helpers/TestContext';
+import { ThError } from '../../../../core/utils/th-responses/ThError';
+import { ThStatusCode, ThResponse } from '../../../../core/utils/th-responses/ThResponse';
+import { UserDO } from '../../../../core/data-layer/hotel/data-objects/user/UserDO';
+import { HotelSignUp } from '../../../../core/domain-layer/hotel-account/sign-up/HotelSignUp';
+import { HotelSignUpDO } from '../../../../core/domain-layer/hotel-account/sign-up/HotelSignUpDO';
+import { ActionTokenDO } from '../../../../core/data-layer/hotel/data-objects/user/ActionTokenDO';
+import { UserAccountActivation } from '../../../../core/domain-layer/hotel-account/account-activation/UserAccountActivation';
+import { UserAccountActivationDO } from '../../../../core/domain-layer/hotel-account/account-activation/UserAccountActivationDO';
+import { UserAccountRequestResetPassword } from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountRequestResetPassword';
+import { UserAccountRequestResetPasswordDO } from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountRequestResetPasswordDO';
+import { UserAccountResetPassword } from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountResetPassword';
+import { UserAccountResetPasswordDO } from '../../../../core/domain-layer/hotel-account/reset-password/UserAccountResetPasswordDO';
+import { UserAccountTestHelper } from "./helpers/UserAccountTestHelper";
+import { HttpStatusCode } from "../../../../core/utils/http/HttpStatusCode";
 
-declare var request: any;
+import request = require('request');
 
-function getSignUpDO(): HotelSignUpDO {
-	var signUpDO: HotelSignUpDO = {
-		email: "dragos.pricope@gmail.com",
-		firstName: "Dragos",
-		lastName: "Pricope",
-		hotelName: "3angleTECH Hotel",
-		password: "TestTest01"
-	};
-	return signUpDO;
-}
+describe("User Account Tests", function () {
+	let testContext: TestContext;
+	let helper: UserAccountTestHelper;
+	let testAccountActivationToken: ActionTokenDO;
 
-describe("User Account Tests", function() {
-    var testContext: TestContext;
-	var testAccountActivationToken: ActionTokenDO;
-	var newPassword = "YyYyzh2718j"
+	let newPassword = "YyYyzh2718j"
 
-	before(function(done: any) {
+	before(function (done: any) {
 		testContext = new TestContext();
+		helper = new UserAccountTestHelper(testContext);
 		done();
-    });
+	});
 
-	describe("Check Sign Up Validations", function() {
-        it("Should return invalid email error", function(done) {
-			var signUpDO = getSignUpDO();
+	describe("Check Sign Up Validations", function () {
+		it("Should return invalid email error", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			signUpDO.email = "invalidemailaggress@ns.invalid";
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
@@ -50,9 +44,9 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.DataEmailValidationError);
 				done();
 			});
-        });
-		it("Should return invalid password error", function(done) {
-			var signUpDO = getSignUpDO();
+		});
+		it("Should return invalid password error", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			signUpDO.password = "tes111111";
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
@@ -61,9 +55,9 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.DataPasswordValidationError);
 				done();
 			});
-        });
-		it("Should return invalid password error", function(done) {
-			var signUpDO = getSignUpDO();
+		});
+		it("Should return invalid password error", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			signUpDO.password = "TEST111111";
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
@@ -72,9 +66,9 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.DataPasswordValidationError);
 				done();
 			});
-        });
-		it("Should return invalid password error", function(done) {
-			var signUpDO = getSignUpDO();
+		});
+		it("Should return invalid password error", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			signUpDO.password = "TESTtest";
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
@@ -83,9 +77,9 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.DataPasswordValidationError);
 				done();
 			});
-        });
-		it("Should return invalid password error", function(done) {
-			var signUpDO = getSignUpDO();
+		});
+		it("Should return invalid password error", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			signUpDO.password = "Tt221";
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
@@ -94,12 +88,12 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.DataPasswordValidationError);
 				done();
 			});
-        });
-    });
+		});
+	});
 
-    describe("Check Sign Up", function() {
-        it("Should return an activation code", function(done) {
-			var signUpDO = getSignUpDO();
+	describe("Check Sign Up", function () {
+		it("Should return an activation code", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
 				testAccountActivationToken = accountActivationToken;
@@ -109,9 +103,9 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done(error._nativeError);
 			});
-        });
-		it("Should return account already exists error code", function(done) {
-			var signUpDO = getSignUpDO();
+		});
+		it("Should return account already exists error code", function (done) {
+			var signUpDO = helper.getSignUpDO();
 			var signUp = new HotelSignUp(testContext.appContext, testContext.sessionContext, signUpDO);
 			signUp.signUp().then((accountActivationToken: ActionTokenDO) => {
 				done(new Error("Signed up with the same email twice!"));
@@ -119,27 +113,26 @@ describe("User Account Tests", function() {
 				should.equal(error.getThStatusCode(), ThStatusCode.HotelRepositoryAccountAlreadyExists);
 				done();
 			});
-        });
-    });
-	describe("Check Account Activation", function() {
-        it("Should not be able to log in using the unactivated account", function(done) {
-			var userData = getSignUpDO();
-			request.post("/api/account/logIn").send({ email: userData.email, password: userData.password })
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					};
-					var response: ThResponse = res.body;
-					should.exist(response);
-					should.notEqual(response.statusCode, ThStatusCode.Ok);
+		});
+	});
+	describe("Check Account Activation", function () {
+		it("Should not be able to log in using the unactivated account", function (done) {
+			
+			var userData = helper.getSignUpDO();
+
+			request(helper.getLoginRequestOptions(userData.email, userData.password), (err, res, body) => {
+				if (res.statusCode !== HttpStatusCode.Ok) {
 					done();
-				});
-        });
-		it("Should not activate the account using the wrong token", function(done) {
+				}
+				else {
+					done(err? err : "Login was successful");
+				}
+			});
+		});
+		it("Should not activate the account using the wrong token", function (done) {
 			var activationDataDO: UserAccountActivationDO = {
 				activationCode: testAccountActivationToken.code + "12334",
-				email: getSignUpDO().email
+				email: helper.getSignUpDO().email
 			};
 			var accountActivation = new UserAccountActivation(testContext.appContext, testContext.sessionContext, activationDataDO);
 			accountActivation.activate().then((user: UserDO) => {
@@ -147,15 +140,16 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done();
 			});
-        });
-		it("Should activate the account", function(done) {
+		});
+		it("Should activate the account", function (done) {
+			
 			var activationDataDO: UserAccountActivationDO = {
 				activationCode: testAccountActivationToken.code,
-				email: getSignUpDO().email
+				email: helper.getSignUpDO().email
 			};
 			var accountActivation = new UserAccountActivation(testContext.appContext, testContext.sessionContext, activationDataDO);
 			accountActivation.activate().then((user: UserDO) => {
-				var signUpData = getSignUpDO();
+				var signUpData = helper.getSignUpDO();
 				should.equal(user.email, signUpData.email);
 				should.notEqual(user.password, signUpData.password);
 				should.equal(user.contactDetails.firstName, signUpData.firstName);
@@ -164,11 +158,12 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done(error._nativeError);
 			});
-        });
-		it("Should not be able to activate the account twice", function(done) {
+		});
+		it("Should not be able to activate the account twice", function (done) {
+			
 			var activationDataDO: UserAccountActivationDO = {
 				activationCode: testAccountActivationToken.code,
-				email: getSignUpDO().email
+				email: helper.getSignUpDO().email
 			};
 			var accountActivation = new UserAccountActivation(testContext.appContext, testContext.sessionContext, activationDataDO);
 			accountActivation.activate().then((user: UserDO) => {
@@ -176,29 +171,27 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done();
 			});
-        });
-		it("Should log in using the activated account", function(done) {
-			var userData = getSignUpDO();
-			request.post("/api/account/logIn").send({ email: userData.email, password: userData.password })
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					};
-					var response: ThResponse = res.body;
-					should.exist(response);
-					should.equal(response.statusCode, ThStatusCode.Ok);
+		});
+		it("Should log in using the activated account", function (done) {
+			
+			var userData = helper.getSignUpDO();
+
+			request(helper.getLoginRequestOptions(userData.email, userData.password), (err, res, body) => {
+				if (res.statusCode === HttpStatusCode.Ok) {
 					done();
-				});
-        });
-    });
+				}
+				else {
+					done(err? err : "Login Failed");
+				}
+			});
 
+		});
+	});
 
-
-	describe("Check Reset Password Flow", function() {
-        it("Should return an activation code to request the password", function(done) {
+	describe("Check Reset Password Flow", function () {
+		it("Should return an activation code to request the password", function (done) {
 			var reqPasswdDO: UserAccountRequestResetPasswordDO = {
-				email: getSignUpDO().email
+				email: helper.getSignUpDO().email
 			}
 			var requestReset = new UserAccountRequestResetPassword(testContext.appContext, testContext.sessionContext, reqPasswdDO);
 			requestReset.requestResetPassword().then((accountActivationToken: ActionTokenDO) => {
@@ -209,10 +202,10 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done(error._nativeError);
 			});
-        });
-		it("Should return another activation code to request the password", function(done) {
+		});
+		it("Should return another activation code to request the password", function (done) {
 			var reqPasswdDO: UserAccountRequestResetPasswordDO = {
-				email: getSignUpDO().email
+				email: helper.getSignUpDO().email
 			}
 			var requestReset = new UserAccountRequestResetPassword(testContext.appContext, testContext.sessionContext, reqPasswdDO);
 			requestReset.requestResetPassword().then((accountActivationToken: ActionTokenDO) => {
@@ -223,24 +216,22 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done(error._nativeError);
 			});
-        });
-		it("Should log in using the old password even though we requested a reset", function(done) {
-			var userData = getSignUpDO();
-			request.post("/api/account/logIn").send({ email: userData.email, password: userData.password })
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					};
-					var response: ThResponse = res.body;
-					should.exist(response);
-					should.equal(response.statusCode, ThStatusCode.Ok);
+		});
+		it("Should log in using the old password even though we requested a reset", function (done) {
+			var userData = helper.getSignUpDO();
+			
+			request(helper.getLoginRequestOptions(userData.email, userData.password), (err, res, body) => {
+				if (res.statusCode === HttpStatusCode.Ok) {
 					done();
-				});
-        });
-		it("Should not change the password using wrong activation code", function(done) {
+				}
+				else {
+					done(err? err : "Login Failed");
+				}
+			});
+		});
+		it("Should not change the password using wrong activation code", function (done) {
 			var resetPasswdDO: UserAccountResetPasswordDO = {
-				email: getSignUpDO().email,
+				email: helper.getSignUpDO().email,
 				activationCode: testAccountActivationToken.code + "Wrong",
 				password: newPassword
 			}
@@ -250,16 +241,16 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done();
 			});
-        });
-		it("Should change the password", function(done) {
+		});
+		it("Should change the password", function (done) {
 			var resetPasswdDO: UserAccountResetPasswordDO = {
-				email: getSignUpDO().email,
+				email: helper.getSignUpDO().email,
 				activationCode: testAccountActivationToken.code,
 				password: newPassword
 			}
 			var resetReset = new UserAccountResetPassword(testContext.appContext, testContext.sessionContext, resetPasswdDO);
 			resetReset.resetPassword().then((user: UserDO) => {
-				var signUpData = getSignUpDO();
+				var signUpData = helper.getSignUpDO();
 				should.equal(user.email, signUpData.email);
 				should.notEqual(user.password, signUpData.password);
 				should.equal(user.contactDetails.firstName, signUpData.firstName);
@@ -268,10 +259,10 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done(error._nativeError);
 			});
-        });
-		it("Should not change the password twice using the same activation code", function(done) {
+		});
+		it("Should not change the password twice using the same activation code", function (done) {
 			var resetPasswdDO: UserAccountResetPasswordDO = {
-				email: getSignUpDO().email,
+				email: helper.getSignUpDO().email,
 				activationCode: testAccountActivationToken.code,
 				password: newPassword
 			}
@@ -281,34 +272,30 @@ describe("User Account Tests", function() {
 			}).catch((error: ThError) => {
 				done();
 			});
-        });
-		it("Should not be able to log in using the old password", function(done) {
-			var userData = getSignUpDO();
-			request.post("/api/account/logIn").send({ email: userData.email, password: userData.password })
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					};
-					var response: ThResponse = res.body;
-					should.exist(response);
-					should.notEqual(response.statusCode, ThStatusCode.Ok);
+		});
+		it("Should not be able to log in using the old password", function (done) {
+			var userData = helper.getSignUpDO();
+			
+			request(helper.getLoginRequestOptions(userData.email, userData.password), (err, res, body) => {
+				if (res.statusCode !== HttpStatusCode.Ok) {
 					done();
-				});
-        });
-		it("Should log in using the new password", function(done) {
-			var userData = getSignUpDO();
-			request.post("/api/account/logIn").send({ email: userData.email, password: newPassword })
-				.expect(200)
-				.end(function(err, res) {
-					if (err) {
-						return done(err)
-					};
-					var response: ThResponse = res.body;
-					should.exist(response);
-					should.equal(response.statusCode, ThStatusCode.Ok);
+				}
+				else {
+					done(err? err : "Login was successful");
+				}
+			});
+		});
+		it("Should log in using the new password", function (done) {
+			var userData = helper.getSignUpDO();
+			
+			request(helper.getLoginRequestOptions(userData.email, newPassword), (err, res, body) => {
+				if (res.statusCode === HttpStatusCode.Ok) {
 					done();
-				});
-        });
-    });
+				}
+				else {
+					done(err? err : "Login Failed");
+				}
+			});
+		});
+	});
 });
---
+++ api/test/root/unit-tests/user-account/helpers/UserAccountTestHelper.ts
@@ -0,0 +1,40 @@
+import { HotelSignUpDO } from "../../../../../core/domain-layer/hotel-account/sign-up/HotelSignUpDO";
+import { TestContext } from "../../../../helpers/TestContext";
+
+import request = require('request');
+
+export class UserAccountTestHelper {
+    private static ClientId: string = "UnitPal-Web";
+    private testContext: TestContext;
+
+    constructor(ctx: TestContext) {
+        this.testContext = ctx;
+    }
+
+    public getSignUpDO(): HotelSignUpDO {
+        var signUpDO: HotelSignUpDO = {
+            email: "dragos.pricope@gmail.com",
+            firstName: "Dragos",
+            lastName: "Pricope",
+            hotelName: "3angleTECH Hotel",
+            password: "TestTest01"
+        };
+        return signUpDO;
+    }
+
+    public getLoginRequestOptions(username: string, password: string) {
+        let clientId = UserAccountTestHelper.ClientId;
+        let baseUrl = this.testContext.appContext.getUnitPalConfig().getAppContextRoot();
+        let url = baseUrl + '/api/token';
+
+        return {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            url: url,
+            body: `client_id=${clientId}&grant_type=password&username=${username}&password=${password}`,
+            method: 'POST',
+        };
+    }
+
+}
