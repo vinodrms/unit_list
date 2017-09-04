70103d62f9707ca8ac5aa714b5b734e32115709b
Dragos Pricope <dragos.pricope@3angle.tech>
2017-09-04 09:42:18 +0300

    #5 Added delete methods in MongoRepository

---
+++ api/core/data-layer/common/base/mongo-utils/MongoDeleteMultipleDocuments.ts
@@ -0,0 +1,27 @@
+import { MongoUtils } from "./MongoUtils";
+
+export class MongoDeleteMultipleDocuments {
+    errorCallback: { (err: Error): void };
+	successCallback: { (deletedCount: number): void };
+
+	constructor(private _sailsEntity: any) {
+	}
+
+	public deleteMultipleDocuments(filter: Object) {
+		var mongoUtils = new MongoUtils();
+		
+		mongoUtils.getNativeMongoCollection(this._sailsEntity).then((collection: any) => {
+			return collection.deleteMany(filter);
+		}).then((result: any) => {
+			if(result.deletedCount !== 1) {
+				this.errorCallback(new Error("Documents could not be removed"));
+				return;
+			}
+			else {
+				this.successCallback(result.deletedCount);
+			}
+		}).catch((error: any) => {
+			this.errorCallback(error);
+		});
+	}
+}
