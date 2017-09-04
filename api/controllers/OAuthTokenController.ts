6b0e5d4c3d53e588e53ad32a456f74d3354a3d63
Dragos Pricope <dragos.pricope@3angle.tech>
2017-09-04 09:50:46 +0300

    #5 Cleanup

--- api/controllers/OAuthTokenController.ts
+++ api/controllers/OAuthTokenController.ts
@@ -3,6 +3,9 @@ import { ThStatusCode } from '../core/utils/th-responses/ThResponse';
 import { AppContext } from '../core/utils/AppContext';
 import { ReportGenerator } from '../core/domain-layer/reports/ReportGenerator';
 import { ReportFileResult } from '../core/domain-layer/reports/common/result/ReportFileResult';
+import { AccessTokenLifetime, RefreshTokenLifetime } from "../core/bootstrap/oauth/OAuthServerInitializer";
+import { TokenDO } from "../core/domain-layer/oauth-tokens/TokenDO";
+import { TokenType } from "../core/domain-layer/oauth-tokens/TokenType";
 
 const OAuth2Server = require('oauth2-server');
 const OAuth2Request = OAuth2Server.Request;
@@ -13,22 +16,24 @@ declare var sails: any;
 export class OAuthTokenController extends BaseController {
     public getToken(req: any, res: any) {
         let options = {
-            // ...
-            // Allow token requests using the password grant to not include a client_secret.
+            // Allow token requests using the password or refresh_token grant to not include a client_secret.
             requireClientAuthentication: {
                 password: false,
-                refresh_token: false
+                refresh_token: false,
+                accessTokenLifetime: AccessTokenLifetime,
+                refreshTokenLifetime: RefreshTokenLifetime
             }
+
         };
-        debugger
 
         sails.config.oauthserver.token(new OAuth2Request(req), new OAuth2Response(res), options)
             .then((token) => {
-                debugger
-                this.returnSuccesfulResponse(req, res, { token: token });
+                let tokenResponse = new TokenDO();
+                tokenResponse.buildFromToken(token, TokenType.Bearer, AccessTokenLifetime);
+
+                this.returnSuccesfulResponse(req, res, tokenResponse);
             }).catch((err: any) => {
-                debugger
-                this.returnErrorResponse(req, res, err, 5);
+                this.returnHttpErrorResponse(req, res, err.code, err.name);
             });
     }
 }
