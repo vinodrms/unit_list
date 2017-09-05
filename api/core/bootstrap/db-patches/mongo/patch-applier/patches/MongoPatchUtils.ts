9a8df2763f6096f3d1b5698cecc1d0994adcad91
Dragos Pricope <dragos.pricope@3angle.tech>
2017-09-05 18:03:43 +0300

    #515 Added patch that enables 20 sign up coded

--- api/core/bootstrap/db-patches/mongo/patch-applier/patches/MongoPatchUtils.ts
+++ api/core/bootstrap/db-patches/mongo/patch-applier/patches/MongoPatchUtils.ts
@@ -7,6 +7,7 @@ import { P4_AddTravelTypeToBookings } from "./list/P4_AddTravelTypeToBookings";
 import { P5_ChangeReservedAddOnProductListStructureForBookings } from "./list/P5_ChangeReservedAddOnProductListStructureForBookings";
 import { P6_AddPaymentDueInDaysToHotels } from "./list/P6_AddPaymentDueInDaysToHotels";
 import { P7_AddIndexForBookingsSort } from "./list/P7_AddIndexForBookingsSort";
+import { P8_AddSignupCodes } from "./list/P8_AddSignupCodes";
 
 export class MongoPatchUtils {
 	public static get PatchList(): ATransactionalMongoPatch[] {
@@ -18,7 +19,9 @@ export class MongoPatchUtils {
 			new P4_AddTravelTypeToBookings(),
 			new P5_ChangeReservedAddOnProductListStructureForBookings(),
 			new P6_AddPaymentDueInDaysToHotels(),
-			new P7_AddIndexForBookingsSort()
+			new P7_AddIndexForBookingsSort(),
+			new P8_AddSignupCodes(),
+			
 		];
 	}
 }
