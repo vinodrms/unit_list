e0932ddea71c1d548a951836cd33189a223f167b
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-10-12 17:36:36 +0300

    #448 Changed the CanCancelDaysBeforePolicy to use an exact timestamp of 00:00 instead of the cancellation hour

--- api/core/data-layer/price-products/data-objects/conditions/cancellation/CanCancelDaysBeforePolicyDO.ts
+++ api/core/data-layer/price-products/data-objects/conditions/cancellation/CanCancelDaysBeforePolicyDO.ts
@@ -1,39 +1,39 @@
-import {BaseDO} from '../../../../common/base/BaseDO';
-import {ThTranslation} from '../../../../../utils/localization/ThTranslation';
-import {IPriceProductCancellationPolicy, PolicyTriggerTimeParams} from './IPriceProductCancellationPolicy';
-import {NumberValidationRule} from '../../../../../utils/th-validation/rules/NumberValidationRule';
-import {CancellationPolicyUtils} from './utils/CancellationPolicyUtils';
-import {ThDateDO} from '../../../../../utils/th-dates/data-objects/ThDateDO';
-import {ThHourDO} from '../../../../../utils/th-dates/data-objects/ThHourDO';
-import {ThDateUtils} from '../../../../../utils/th-dates/ThDateUtils';
-import {BookingStateChangeTriggerTimeDO, BookingStateChangeTriggerType} from '../../../../bookings/data-objects/state-change-time/BookingStateChangeTriggerTimeDO';
+import { BaseDO } from '../../../../common/base/BaseDO';
+import { ThTranslation } from '../../../../../utils/localization/ThTranslation';
+import { IPriceProductCancellationPolicy, PolicyTriggerTimeParams } from './IPriceProductCancellationPolicy';
+import { NumberValidationRule } from '../../../../../utils/th-validation/rules/NumberValidationRule';
+import { CancellationPolicyUtils } from './utils/CancellationPolicyUtils';
+import { ThDateDO } from '../../../../../utils/th-dates/data-objects/ThDateDO';
+import { ThHourDO } from '../../../../../utils/th-dates/data-objects/ThHourDO';
+import { ThDateUtils } from '../../../../../utils/th-dates/ThDateUtils';
+import { BookingStateChangeTriggerTimeDO, BookingStateChangeTriggerType } from '../../../../bookings/data-objects/state-change-time/BookingStateChangeTriggerTimeDO';
 
 export class CanCancelDaysBeforePolicyDO extends BaseDO implements IPriceProductCancellationPolicy {
-	daysBefore: number;
+    daysBefore: number;
 
-	protected getPrimitivePropertyKeys(): string[] {
-		return ["daysBefore"];
-	}
+    protected getPrimitivePropertyKeys(): string[] {
+        return ["daysBefore"];
+    }
 
-	public hasCancellationPolicy(): boolean {
-		return true;
-	}
-	public isValid(): boolean {
-		var rule = NumberValidationRule.buildIntegerNumberRule(0);
-		return rule.validate(this.daysBefore, "daysBefore").isValid();
-	}
-	public getValueDisplayString(thTranslation: ThTranslation): string {
-		return thTranslation.translate("Can cancel %daysBefore% days before arrival", { daysBefore: this.daysBefore });
-	}
+    public hasCancellationPolicy(): boolean {
+        return true;
+    }
+    public isValid(): boolean {
+        var rule = NumberValidationRule.buildIntegerNumberRule(0);
+        return rule.validate(this.daysBefore, "daysBefore").isValid();
+    }
+    public getValueDisplayString(thTranslation: ThTranslation): string {
+        return thTranslation.translate("Can cancel %daysBefore% days before arrival", { daysBefore: this.daysBefore });
+    }
 
-	public generateGuaranteedTriggerTime(triggerParams: PolicyTriggerTimeParams): BookingStateChangeTriggerTimeDO {
-		var ccUtils = new CancellationPolicyUtils();
-		var cancelDateDO = triggerParams.arrivalDate.buildPrototype();
-		cancelDateDO = ccUtils.thDateUtils.addDaysToThDateDO(cancelDateDO, (-1 * this.daysBefore));
-		return ccUtils.generateStateChangeTriggerTimeDO(BookingStateChangeTriggerType.DependentOnCancellationHour, cancelDateDO, ThHourDO.buildThHourDO(0, 0));
-	}
-	public generateNoShowTriggerTime(triggerParams: PolicyTriggerTimeParams): BookingStateChangeTriggerTimeDO {
-		var ccUtils = new CancellationPolicyUtils();
-		return ccUtils.generateMidnightStateChangeTriggerTimeDO(triggerParams.arrivalDate);
-	}
+    public generateGuaranteedTriggerTime(triggerParams: PolicyTriggerTimeParams): BookingStateChangeTriggerTimeDO {
+        var ccUtils = new CancellationPolicyUtils();
+        var cancelDateDO = triggerParams.arrivalDate.buildPrototype();
+        cancelDateDO = ccUtils.thDateUtils.addDaysToThDateDO(cancelDateDO, (-1 * this.daysBefore));
+        return ccUtils.generateStateChangeTriggerTimeDO(BookingStateChangeTriggerType.ExactTimestamp, cancelDateDO, ThHourDO.buildThHourDO(0, 0));
+    }
+    public generateNoShowTriggerTime(triggerParams: PolicyTriggerTimeParams): BookingStateChangeTriggerTimeDO {
+        var ccUtils = new CancellationPolicyUtils();
+        return ccUtils.generateMidnightStateChangeTriggerTimeDO(triggerParams.arrivalDate);
+    }
 }
