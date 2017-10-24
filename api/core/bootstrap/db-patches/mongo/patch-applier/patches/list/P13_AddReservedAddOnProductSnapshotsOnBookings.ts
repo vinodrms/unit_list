e2d81bd61bc7e7dff96444eba7feb5fafb0c17ed
Ionut Cristian Paraschiv <ionut.paraschiv@3angle.tech>
2017-10-25 00:44:23 +0300

    #513 Fixed bookings' migration script so that it can run more than once

--- api/core/bootstrap/db-patches/mongo/patch-applier/patches/list/P13_AddReservedAddOnProductSnapshotsOnBookings.ts
+++ api/core/bootstrap/db-patches/mongo/patch-applier/patches/list/P13_AddReservedAddOnProductSnapshotsOnBookings.ts
@@ -9,16 +9,8 @@ import { ThError } from "../../../../../../utils/th-responses/ThError";
 
 export class P13_AddReservedAddOnProductSnapshotsOnBookings extends APaginatedTransactionalMongoPatch {
 
-    private addOnProductRepository: MongoAddOnProductRepository;
-
     protected getMongoRepository(): MongoRepository {
         return this.bookingRepository;
-    }
-
-
-    constructor() {
-        super();
-        this.addOnProductRepository = new MongoAddOnProductRepository();
     }
 
     public getPatchType(): MongoPatchType {
@@ -26,25 +18,33 @@ export class P13_AddReservedAddOnProductSnapshotsOnBookings extends APaginatedTr
     }
 
     protected updateDocumentInMemoryAsyncCore(resolve: { (result: any): void }, reject: { (err: ThError): void }, booking) {
-        var reservedAddOnProductIdList = _.chain(booking.reservedAddOnProductList).map((reservedAddOnProduct) => {return reservedAddOnProduct.aopId}).uniq().compact().value();
+        var reservedAddOnProductIdList = [];
+        if (_.isArray(booking.reservedAddOnProductList)) {
+            booking.reservedAddOnProductList.forEach(reservedAddOnProduct => {
+                if (_.isString(reservedAddOnProduct.aopId) && reservedAddOnProduct.aopId.length > 0) {
+                    reservedAddOnProductIdList.push(reservedAddOnProduct.aopId);
+                }
+            });
+        }
+        reservedAddOnProductIdList = _.uniq(reservedAddOnProductIdList);
         var aopList: AddOnProductDO[] = [];
         if (_.isArray(reservedAddOnProductIdList) && reservedAddOnProductIdList.length > 0) {
-		this.addOnProductRepository.getAddOnProductList({ hotelId: booking.hotelId }, { addOnProductIdList: reservedAddOnProductIdList })
-			.then((searchResult: AddOnProductSearchResultRepoDO) => {
-                aopList = searchResult.addOnProductList;
-                _.forEach(booking.reservedAddOnProductList, (reservedAddOnProduct) => {
-                    var aopWithSameId: AddOnProductDO = _.find(aopList, (aop: AddOnProductDO) => {return aop.id === reservedAddOnProduct.aopId});
-                    reservedAddOnProduct.aopSnapshot = aopWithSameId.getAddOnProductSnapshotDO();
-                    delete reservedAddOnProduct.aopId;
+            this.addOnProductRepository.getAddOnProductList({ hotelId: booking.hotelId }, { addOnProductIdList: reservedAddOnProductIdList })
+                .then((searchResult: AddOnProductSearchResultRepoDO) => {
+                    aopList = searchResult.addOnProductList;
+                    _.forEach(booking.reservedAddOnProductList, (reservedAddOnProduct) => {
+                        var aopWithSameId: AddOnProductDO = _.find(aopList, (aop: AddOnProductDO) => { return aop.id === reservedAddOnProduct.aopId });
+                        reservedAddOnProduct.aopSnapshot = aopWithSameId.getAddOnProductSnapshotDO();
+                        delete reservedAddOnProduct.aopId;
+                    });
+                    resolve(booking);
+                }).catch((error: ThError) => {
+                    reject(error);
                 });
-                resolve(booking);
-            }).catch((error: ThError) => {
-                reject(error);
-            });
         } else {
             resolve(booking);
         };
     }
 
-    protected updateDocumentInMemory(booking) {}
+    protected updateDocumentInMemory(booking) { }
 }
