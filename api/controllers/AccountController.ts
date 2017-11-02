e61436604004b0adc2402e2aeffd8fb410ce9d46
Dragos Pricope <dragos.pricope@3angle.tech>
2017-11-02 14:17:19 +0200

    #555 On logout invalidate the token by access token, not by user id

--- api/controllers/AccountController.ts
+++ api/controllers/AccountController.ts
@@ -13,63 +13,63 @@ import { UserAccountResetPassword } from '../core/domain-layer/hotel-account/res
 import { LoginStatusCode } from '../core/utils/th-responses/LoginStatusCode';
 
 class AccountController extends BaseController {
-	public isAuthenticated(req: any, res: any) {
-		this.returnSuccesfulResponse(req, res, {});
-	}
-	public signUp(req: any, res: any) {
-		var hotelSignUp = new HotelSignUp(req.appContext, req.sessionContext, req.body.accountData);
-		hotelSignUp.signUp().then((result: ActionTokenDO) => {
-			this.returnSuccesfulResponse(req, res, {});
-		}).catch((err: any) => {
-			this.returnErrorResponse(req, res, err, ThStatusCode.HotelSignUpError);
-		});
-	}
-	public activate(req: any, res: any) {
-		var appContext: AppContext = req.appContext;
-		var hotelActivateAccount = new UserAccountActivation(req.appContext, req.sessionContext, req.query);
-		hotelActivateAccount.activate().then((result: any) => {
-			res.redirect(appContext.getUnitPalConfig().getAppContextRoot() + "/login/" + LoginStatusCode.AccountActivationOk);
-		}).catch((err: any) => {
-			res.redirect(appContext.getUnitPalConfig().getAppContextRoot() + "/login/" + LoginStatusCode.AccountActivationError);
-		});
-	}
-	public logOut(req: any, res: any) {
-		let appContext: AppContext = req.appContext;
-		let sessionContext: SessionContext = req.sessionContext;
-
-		let tokenService = appContext.getServiceFactory().getTokenService();
-		tokenService.revokeAllTokensByUser(
-			sessionContext.sessionDO.user.id
-		).then((deletedCount: number) => {
-			this.returnSuccesfulResponse(req, res, {});
-		}).catch((error: any) => {
-			this.returnErrorResponse(req, res, error, ThStatusCode.UserLogoutError);
-		});
-	}
-	public requestResetPassword(req: any, res: any) {
-		var reqPasswd = new UserAccountRequestResetPassword(req.appContext, req.sessionContext, req.body.requestData);
-		reqPasswd.requestResetPassword().then((resetToken: ActionTokenDO) => {
-			this.returnSuccesfulResponse(req, res, {});
-		}).catch((err: any) => {
-			this.returnErrorResponse(req, res, err, ThStatusCode.AccControllerErrorRequestingResetPassword);
-		});
-	}
-	public resetPassword(req: any, res: any) {
-		var reqPasswd = new UserAccountResetPassword(req.appContext, req.sessionContext, req.body.requestData);
-		reqPasswd.resetPassword().then((user: UserDO) => {
-			this.returnSuccesfulResponse(req, res, {});
-		}).catch((err: any) => {
-			this.returnErrorResponse(req, res, err, ThStatusCode.AccControllerErrorResettingPassword);
-		});
-	}
+    public isAuthenticated(req: any, res: any) {
+        this.returnSuccesfulResponse(req, res, {});
+    }
+    public signUp(req: any, res: any) {
+        var hotelSignUp = new HotelSignUp(req.appContext, req.sessionContext, req.body.accountData);
+        hotelSignUp.signUp().then((result: ActionTokenDO) => {
+            this.returnSuccesfulResponse(req, res, {});
+        }).catch((err: any) => {
+            this.returnErrorResponse(req, res, err, ThStatusCode.HotelSignUpError);
+        });
+    }
+    public activate(req: any, res: any) {
+        var appContext: AppContext = req.appContext;
+        var hotelActivateAccount = new UserAccountActivation(req.appContext, req.sessionContext, req.query);
+        hotelActivateAccount.activate().then((result: any) => {
+            res.redirect(appContext.getUnitPalConfig().getAppContextRoot() + "/login/" + LoginStatusCode.AccountActivationOk);
+        }).catch((err: any) => {
+            res.redirect(appContext.getUnitPalConfig().getAppContextRoot() + "/login/" + LoginStatusCode.AccountActivationError);
+        });
+    }
+    public logOut(req: any, res: any) {
+        let appContext: AppContext = req.appContext;
+        let sessionContext: SessionContext = req.sessionContext;
+        debugger
+        let tokenService = appContext.getServiceFactory().getTokenService();
+        tokenService.invalidateToken({
+            accessToken: sessionContext.token.accessToken
+        }).then((deletedCount: number) => {
+            this.returnSuccesfulResponse(req, res, {});
+        }).catch((error: any) => {
+            this.returnErrorResponse(req, res, error, ThStatusCode.UserLogoutError);
+        });
+    }
+    public requestResetPassword(req: any, res: any) {
+        var reqPasswd = new UserAccountRequestResetPassword(req.appContext, req.sessionContext, req.body.requestData);
+        reqPasswd.requestResetPassword().then((resetToken: ActionTokenDO) => {
+            this.returnSuccesfulResponse(req, res, {});
+        }).catch((err: any) => {
+            this.returnErrorResponse(req, res, err, ThStatusCode.AccControllerErrorRequestingResetPassword);
+        });
+    }
+    public resetPassword(req: any, res: any) {
+        var reqPasswd = new UserAccountResetPassword(req.appContext, req.sessionContext, req.body.requestData);
+        reqPasswd.resetPassword().then((user: UserDO) => {
+            this.returnSuccesfulResponse(req, res, {});
+        }).catch((err: any) => {
+            this.returnErrorResponse(req, res, err, ThStatusCode.AccControllerErrorResettingPassword);
+        });
+    }
 }
 
 var accountController = new AccountController();
 module.exports = {
-	isAuthenticated: accountController.isAuthenticated.bind(accountController),
-	signUp: accountController.signUp.bind(accountController),
-	activate: accountController.activate.bind(accountController),
-	logOut: accountController.logOut.bind(accountController),
-	requestResetPassword: accountController.requestResetPassword.bind(accountController),
-	resetPassword: accountController.resetPassword.bind(accountController)
+    isAuthenticated: accountController.isAuthenticated.bind(accountController),
+    signUp: accountController.signUp.bind(accountController),
+    activate: accountController.activate.bind(accountController),
+    logOut: accountController.logOut.bind(accountController),
+    requestResetPassword: accountController.requestResetPassword.bind(accountController),
+    resetPassword: accountController.resetPassword.bind(accountController)
 }
